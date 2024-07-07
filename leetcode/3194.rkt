1b744709da380d2035bdcfc22d5029de6add95a5
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-07 14:46:21 -0700

    ...

---
+++ leetcode/3194.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (minimum-average nums)
+  (letrec ((iter (lambda (sorted-nums acc)
+                   (if (null? sorted-nums)
+                       acc
+                       (let ((n (length sorted-nums))
+                             (low (first sorted-nums))
+                             (high (last sorted-nums)))
+                         (iter
+                          (take (drop sorted-nums 1) (- n 2))
+                          (cons (/ (+ low high) 2.0) acc)))))))
+    (apply min
+     (iter (sort nums <) '()))))
+
+
+(minimum-average '(7 8 3 4 15 13 4 1))
+(minimum-average '(1 9 8 3 10 5))
+(minimum-average '(1 2 3 7 8 9))
