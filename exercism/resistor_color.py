e76d34312ea524510d742121831d1a0778c7f6a6
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-15 07:12:46 -0700

    ...

---
+++ exercism/resistor_color.py
@@ -0,0 +1,20 @@
+color_values = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+
+def color_code(color):
+    return color_values[color]
+
+
+def colors():
+    return list(color_values.keys())
