a16bc5ce0a4a47af35a9f05ce268c6cb05e9e1d9
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-15 20:52:43 -0700

    ...

--- exercism/resistor_color.py
+++ exercism/resistor_color.py
@@ -1,15 +1,15 @@
-color_values = {
-    "black": 0,
-    "brown": 1,
-    "red": 2,
-    "orange": 3,
-    "yellow": 4,
-    "green": 5,
-    "blue": 6,
-    "violet": 7,
-    "grey": 8,
-    "white": 9,
-}
+color_values = dict(
+    black=0,
+    brown=1,
+    red=2,
+    orange=3,
+    yellow=4,
+    green=5,
+    blue=6,
+    violet=7,
+    grey=8,
+    white=9,
+)
 
 
 def color_code(color):
