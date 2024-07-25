458da2036541d3d4483d2becf706aeb86b113e48
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-25 08:36:16 -0700

    code clean up

--- leetcode/2191.rkt
+++ leetcode/2191.rkt
@@ -1,27 +1,33 @@
+; Create an association list of the numbers and their mapped values and sort by
+; the mapped values. Remove the mapped values and return the numbers in the
+; updated order.
+
 (define (sort-jumbled mapping nums)
   (let* ((map-number (lambda (num) ; int -> int
                        (let* ((char->digit (lambda (c)
                                              (- (char->integer c)
                                                 (char->integer #\0))))
                               (digit->char (lambda (digit)
-                                             (integer->char (+ digit
-                                                               (char->integer #\0)))))
+                                             (integer->char
+                                              (+ digit
+                                                 (char->integer #\0)))))
                               (number->digits (lambda (num)
-                                                (map char->digit (string->list
-                                                                  (number->string num)))))
+                                                (map
+                                                 char->digit
+                                                 (string->list
+                                                   (number->string num)))))
                               (digits->number (lambda (digits)
-                                                (string->number (list->string (map digit->char digits)))))
+                                                (string->number
+                                                 (list->string
+                                                  (map digit->char digits)))))
                               (digits (number->digits num))
                               (mapped-digits (map
                                               (lambda (digit)
                                                 (list-ref mapping digit))
                                               digits)))
                          (digits->number mapped-digits))))
-         (map-pairs (map
-                     (lambda (num)
-                       (cons num
-                             (map-number num)))
-                     nums))
+         (map-pairs (map cons nums
+                              (map map-number nums)))
          (sorted-pairs (sort ; sort map pairs by mapped nums
                         map-pairs
                         (lambda (pair-a pair-b)
