311527ea00703f8535f8605c9587ac10acc92b1e
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-24 21:54:21 -0700

    improved implementation of core algorithm

--- leetcode/2191.rkt
+++ leetcode/2191.rkt
@@ -12,28 +12,29 @@
 
 
 (define (sort-jumbled mapping nums)
-  (let ((map-num (lambda (num)
-                   (let ((digits (map
-                                  (lambda (c)
-                                    (- (char->integer c)
-                                       (char->integer #\0)))
-                                  (string->list (number->string num)))))
-                     (foldl
-                      (lambda (pair acc)
-                        (let ((index (car pair))
-                              (converted-digit (cadr pair)))
-                          (+ acc
-                             (* converted-digit (expt 10 index)))))
-                      0
-                      (zip
-                       (range (length digits))
-                       (reverse (map
-                                 (lambda (digit)
-                                   (list-ref mapping digit))
-                                 digits)))))))
-        ((mapped-nums (map map-num nums))))
-    (map car (sort ; sort pairs by mapped nums
-              (zip nums mapped-nums) ; pairs
-              (lambda (pair-1 pair-2)
-                (< (cadr pair-1)
-                   (cadr pair-2)))))))
+  (let* ((map-num (lambda (num)
+                    (let ((digits (map
+                                   (lambda (c)
+                                     (- (char->integer c)
+                                        (char->integer #\0)))
+                                   (string->list (number->string num)))))
+                      (foldl
+                       (lambda (pair acc)
+                         (let ((index (car pair))
+                               (converted-digit (cadr pair)))
+                           (+ acc (* converted-digit (expt 10 index)))))
+                       0
+                       (zip
+                        (range (length digits))
+                        (reverse (map
+                                  (lambda (digit)
+                                    (list-ref mapping digit))
+                                  digits)))))))
+         (mapped-nums (map map-num nums))
+         (map-pairs (zip nums mapped-nums))
+         (sorted-pairs (sort ; sort map pairs by mapped nums
+                        map-pairs
+                        (lambda (pair-a pair-b)
+                          (< (cadr pair-a)
+                             (cadr pair-b))))))
+    (map car sorted-pairs)))
