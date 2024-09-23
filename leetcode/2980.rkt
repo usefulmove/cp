8f3688fca8a3c8854c07f9c9e3b7674eb243ab6c
Duane Edmonds <duane.edmonds@gmail.com>
2024-09-23 13:27:59 -0700

    ...

---
+++ leetcode/2980.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (has-trailing-zeros nums)
+  (let ((unset-low-bits (foldl
+                         (lambda (num acc)
+                           (+ acc (bitwise-and num 1)))
+                         0
+                         nums)))
+    #;(> unset-low-bits 1)
+    (> (- (length nums) unset-low-bits) 1)))
+
+(has-trailing-zeros '(1 2 3 4 5))
+(has-trailing-zeros '(2 4 8 16))
+(has-trailing-zeros '(1 3 5 7 9))
