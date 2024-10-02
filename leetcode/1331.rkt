22c2ee449e69f8263345830e7e485c61fb277b92
Duane Edmonds <duane.edmonds@gmail.com>
2024-10-02 11:50:02 -0700

    ...

--- leetcode/1331.rkt
+++ leetcode/1331.rkt
@@ -3,17 +3,18 @@
 (define (array-rank-transform vals)
   (let* ((value-rank-assoc (foldl ; rank assoc :: [int . int]
                             (lambda (val acc-so-far)
-                              (cond ((null? acc-so-far) (list (cons val 1)))
-                                    ((assoc val acc-so-far) acc-so-far)
-                                    (else ; add val
-                                          (map ; map from rank to value using rank assoc
-                                           (lambda (rank)
-                                             (if (todo)
-                                              ; rank exists in value-rank association so far                                                 (if (< rank val)
-                                                     (cons rank (assoc rank acc-so-far))
-                                                     (cons rank (+ 1 (assoc rank acc-so-far))))
-                                                 (cons (cons val rank) acc-so-far)))
-                                           (range 1 (+ 1 (length acc-so-far)))))))
+                              (let ((rv-acc-so-far (todo-transpose-assoc acc-so-far)))
+                                (cond ((null? acc-so-far) (list (cons val 1)))
+                                      ((assoc val acc-so-far) acc-so-far)
+                                      (else ; add val
+                                       (map ; map from rank to value using rank assoc
+                                        (lambda (rank)
+                                          (let ((value-for-rank? (assoc rank rv-acc-so-far)))
+                                            ; is there a value associated with this rank?
+                                            (if value-for-rank? ; rank exists?
+                                                (cons value-for-rank? (if (< rank )))
+                                                (cons (cons val rank) acc-so-far))))
+                                       (range 1 (+ 1 (length acc-so-far))))))))
                             '() ; value-rank assoc accumulator [value . rank] :: [int . int]
                             vals))
          (value->rank (lambda (value) (assoc value value-rank-assoc)))) ; :: (int -> int)
