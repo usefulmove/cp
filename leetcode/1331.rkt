375c5d5b5c6967973396a98ab884b04fca17b6f1
Duane Edmonds <duane.edmonds@gmail.com>
2024-10-01 19:20:07 -0700

    ...

--- leetcode/1331.rkt
+++ leetcode/1331.rkt
@@ -1,10 +1,12 @@
 #lang racket
 
 (define (array-rank-transform arr)
-  (let ((rank-assoc todo) ; rank association :: [int]
-        (learn-assoc (lambda (value rank-assoc) ; (int -> [int] -> (int -> int))
-                       todo-return-rank))
-        (value->rank (learn-assoc rank-assoc))) ; (int -> int)
+  (let* ((rank-assoc (foldl
+                      (lambda (n acc)
+                        todo)
+                      '()
+                      arr)) ; rank association :: [int . int]
+         (value->rank (lambda (value) (assoc value rank-assoc)))) ; (int -> int)
     (map
      value->rank
      arr)))
