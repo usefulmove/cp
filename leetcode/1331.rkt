80af23d2276a386675a558d27eda6353795e3090
Duane Edmonds <duane.edmonds@gmail.com>
2024-10-02 22:11:02 -0700

    ...

--- leetcode/1331.rkt
+++ leetcode/1331.rkt
@@ -3,23 +3,46 @@
 (define (array-rank-transform vals)
   (let* ((value-rank-assoc (foldl ; rank assoc :: [int . int]
                             (lambda (val acc-so-far)
-                              (let ((rv-acc-so-far (todo-transpose-assoc acc-so-far)))
+                              (let ((rv-acc-so-far (map
+                                                    (lambda (pair)
+                                                      (cons (cdr pair) (car pair)))
+                                                    acc-so-far)))
                                 (cond ((null? acc-so-far) (list (cons val 1)))
                                       ((assoc val acc-so-far) acc-so-far)
                                       (else ; add val
+                                       #;(print "acc: ")
+                                       #;(print "adding ")
+                                       #;(print val)
+                                       #;(print " to ")
+                                       #;(println acc-so-far)
+                                       #;(println (range 1 (+ 2 (length acc-so-far))))
                                        (map ; map from rank to value using rank assoc
                                         (lambda (rank)
-                                          (let ((value-for-rank? (assoc rank rv-acc-so-far)))
+                                          (let ((value-for-rank-pair? (assoc rank rv-acc-so-far)))
                                             ; is there a value associated with this rank?
-                                            (if value-for-rank? ; rank exists?
-                                                (cons value-for-rank? (if (< rank )))
-                                                (cons (cons val rank) acc-so-far))))
-                                       (range 1 (+ 1 (length acc-so-far))))))))
+                                            (if value-for-rank-pair? ; value exists?
+                                                (let ((value-for-rank (cdr value-for-rank-pair?)))
+                                                  (cons value-for-rank
+                                                        (if (< value-for-rank val)
+                                                            rank
+                                                            (+ 1 rank))))
+                                                (let* ((ranked-values (map car acc-so-far))
+                                                       (new-rank (+ 1 (count
+                                                                       (lambda (n) (< n val))
+                                                                       ranked-values))))
+                                                  (cons val new-rank)))))
+                                        (range 1 (+ 2 (length acc-so-far))))))))
                             '() ; value-rank assoc accumulator [value . rank] :: [int . int]
                             vals))
-         (value->rank (lambda (value) (assoc value value-rank-assoc)))) ; :: (int -> int)
+         (value->rank (lambda (value) ; :: (int -> int)
+                        (cdr (assoc value value-rank-assoc)))))
+    ;(println vals)
+    ;(println value-rank-assoc)
     (map
      value->rank
      vals)))
 
-(array-rank-transform '(100 200 300))
+#;(array-rank-transform '(100 100 100))
+#;(array-rank-transform '(100 200 300))
+#;(array-rank-transform '(400 100 300 200))
+#;(array-rank-transform '(37 12 28 9 100 56 80 5 12))
