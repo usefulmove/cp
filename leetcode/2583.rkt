027fbd81f2c0dc3a651ebbe9bc3cfabd8d18a9fb
Duane Edmonds <duane.edmonds@gmail.com>
2024-10-23 00:55:16 -0700

    ...

---
+++ leetcode/2583.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define (kth-largest-level-sum root k)
+  (letrec ((level-sums '()) ; (level . sum) assoc list
+           (update-level-sums
+            (lambda (node level)
+              (cond ((not (tree-node? node)) (void))
+                    ((null? node) (void))
+                    (else (let* ((node-value (tree-node-val node))
+                                 (level-pair (assoc level level-sums)))
+                            (if level-pair
+                                (set! level-sums (map
+                                                  (lambda (pair)
+                                                    (if (= level (car pair))
+                                                        (cons level (+ node-value
+                                                                       (cdr level-pair)))
+                                                        pair))
+                                                  level-sums))
+                                (set! level-sums (cons (cons level node-value)
+                                                       level-sums)))
+                            (update-level-sums (tree-node-left node) (+ 1 level))
+                            (update-level-sums (tree-node-right node) (+ 1 level))))))))
+
+    (update-level-sums root 1) ; updates level-sums
+
+    (if (> k (length level-sums))
+        -1
+        (let ((sorted-sums (sort
+                            (map cdr level-sums)
+                            >)))
+          (list-ref sorted-sums (- k 1))))))
