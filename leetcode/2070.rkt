fc1bd8932b1655555c8d02b6672ea16fc40b17fe
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-12 14:40:25 -0800

    ...

--- leetcode/2070.rkt
+++ leetcode/2070.rkt
@@ -1,5 +1,36 @@
 #lang racket
 
+
+(define (maximum-beauty items queries)
+  (map
+   (lambda (query)
+     (foldl
+      (lambda (item acc-max-so-far)
+        (max acc-max-so-far
+             (if (<= (first item) query)
+                 (second item)
+                 0)))
+      0
+      items))
+   queries))
+
+
+(define (maximum-beauty items queries)
+  (map
+   (lambda (query)
+     (foldl
+      (lambda (item acc-max-so-far)
+        (max acc-max-so-far
+             (let ((price (first item))
+                   (beauty (second item)))
+               (if (<= price query)
+                   beauty
+                   0))))
+      0
+      items))
+   queries))
+
+
 (define (maximum-beauty items queries)
   (map
    (lambda (query)
