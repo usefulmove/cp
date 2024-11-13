4804eb8cab8731b067c22817aeeb43ae4b7c8f71
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-13 10:48:42 -0800

    ...

--- leetcode/2563.rkt
+++ leetcode/2563.rkt
@@ -1,17 +1,18 @@
 #lang racket
 
 (define (count-fair-pairs nums lower upper)
-  (let ((len (length nums))
-        (count 0))
-    (for ((i (range (- len 1))))
-      (for ((j (range (+ i 1) len)))
-        (let ((sum (+ (list-ref nums i)
-                      (list-ref nums j))))
-          (if (and (>= sum lower)
-                   (<= sum upper))
-              (set! count (+ count 1))
-              (void)))))
-    count))
+  (let loop ((ns nums)
+             (cnt 0))
+    (if (empty? ns)
+        cnt
+        (loop (cdr ns)
+              (+ cnt (count
+                      (lambda (m)
+                        (let* ((n (car ns))
+                               (sum (+ n m)))
+                          (and (>= sum lower)
+                               (<= sum upper))))
+                      (cdr ns)))))))
 
 (count-fair-pairs '(0 1 7 4 4 5) 3 6)
 (count-fair-pairs '(1 7 9 2 5) 11 11)
