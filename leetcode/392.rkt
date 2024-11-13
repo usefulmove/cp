6a8bb8cdd8186fc3b82509bb25a9b28eed10c03a
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-13 09:02:47 -0800

    ...

--- leetcode/392.rkt
+++ leetcode/392.rkt
@@ -5,6 +5,19 @@
         (t-chars (string->list t)))
     (let loop ((scs s-chars)
                (tcs t-chars))
+      (cond ((empty? scs) #t) ; all characters were present (in order)
+             (else (let* ((c (car scs))
+                          (mbr (member c tcs)))
+                     (if mbr ; if character is present in list ...
+                         (loop (cdr scs) ; continue checking
+                               (cdr mbr))
+                         #f))))))) ; not subsequence
+
+(define (is-subsequence s t)
+  (let ((s-chars (string->list s))
+        (t-chars (string->list t)))
+    (let loop ((scs s-chars)
+               (tcs t-chars))
       (cond ((empty? scs) #t)
             ((member (car scs) tcs) (loop (cdr scs)
                                           (cdr (member (car scs) tcs))))
