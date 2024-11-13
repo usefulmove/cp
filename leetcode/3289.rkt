4a9daf0294ee835e246de269c103e1efecc5f5ad
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-13 00:46:31 -0800

    ...

--- leetcode/3289.rkt
+++ leetcode/3289.rkt
@@ -1,12 +1,24 @@
 #lang racket
 
 (define (get-sneaky-numbers nums)
+  (let loop ((lst nums)
+             (result '())
+             (seen '()))
+    (cond ((empty? lst) result)
+          ((member (car lst) seen) (loop (cdr lst)
+                                         (cons (car lst) result)
+                                         seen))
+          (else (loop (cdr lst)
+                      result
+                      (cons (car lst) seen))))))
+
+(define (get-sneaky-numbers nums)
   (car (foldl
         (lambda (num acc)
-          (let ((out (car acc))
+          (let ((result (car acc))
                 (seen (cdr acc)))
-            (if (member num seen)
-                (cons (cons num out) seen)
-                (cons out (cons num seen)))))
-        '(() . ()) ; (out . seen)
+            (if (member num seen) ; if seen previously ...
+                (cons (cons num result) seen) ; add to result
+                (cons result (cons num seen))))) ; otherwise, add to seen
+        '(() . ()) ; (result . seen)
         nums)))
