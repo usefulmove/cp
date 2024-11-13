d4f212bccc56c744326ab98f54a29feb4a691324
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-13 11:52:56 -0800

    ...

--- leetcode/2563.rkt
+++ leetcode/2563.rkt
@@ -1,23 +1,30 @@
 #lang racket
 
+(define (memoize f)
+  (define cache (make-hash))
+  (lambda (x)
+    (hash-ref! cache x (lambda ()
+                         (f x)))))
+
 (define (count-fair-pairs nums lower upper)
-  (let loop ((ns nums)
-             (cnt 0))
-    (if (empty? ns)
-        cnt
-        (let ((n (car ns))
-              (rst (cdr ns)))
-          (loop rst
-                (+ cnt (let recur ((lst rst)
-                                   (ccnt 0))
-                         (if (empty? lst)
-                             ccnt
-                             (recur (cdr lst)
-                                    (+ ccnt (let* ((m (car lst))
-                                                   (sm (+ m n)))
-                                              (if (and (>= sm lower)
-                                                       (<= sm upper))
-                                                  1 0))))))))))))
+  (let ((sum-fair? (memoize (lambda (s)
+                              (and (>= s lower)
+                                   (<= s upper))))))
+    (let loop ((ns nums)
+               (cnt 0))
+      (if (empty? ns)
+          cnt
+          (let ((n (car ns))
+                (rst (cdr ns)))
+            (loop rst
+                  (+ cnt (let recur ((lst rst)
+                                     (ccnt 0))
+                           (if (empty? lst)
+                               ccnt
+                               (recur (cdr lst)
+                                      (+ ccnt (let* ((m (car lst))
+                                                     (sm (+ m n)))
+                                                (if (sum-fair? sm) 1 0)))))))))))))
 
 (define (count-fair-pairs nums lower upper)
   (let loop ((ns nums)
