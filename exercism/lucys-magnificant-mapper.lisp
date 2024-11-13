4f8747c28b81f114e76a7ac67a3a6aaef6b9ca13
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-12 21:38:57 -0800

    ...

---
+++ exercism/lucys-magnificant-mapper.lisp
@@ -0,0 +1,15 @@
+(defpackage :lucys-magnificent-mapper
+  (:use :cl)
+  (:export :make-magnificent-maybe :only-the-best))
+
+(in-package :lucys-magnificent-mapper)
+
+(defun make-magnificent-maybe (f nums)
+  (mapcar f nums))
+
+(defun only-the-best (f nums)
+  (remove-if
+   (lambda (num)
+     (or (= 1 num)
+         (funcall f num)))
+   nums))
