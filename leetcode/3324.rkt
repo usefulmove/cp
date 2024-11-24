e14843aec887771f725798d5e09bcb9d36bc0413
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-24 13:56:59 -0800

    ...

--- leetcode/3324.rkt
+++ leetcode/3324.rkt
@@ -1,9 +1,17 @@
 #lang racket
 
 (define (string-sequence target)
-  (let loop ((todo todo))
-    todo))
+  (let (next-char (lambda (c) (integer->char
+                               (+ (char->integer c) 1))))
+    (let loop ((cs (string->list target))
+               (output '()))
+      (if (empty? cs)
+          (reverse output)
+          (let ((char-to-match (car cs)))
+            (if (empty? output-head)
+                (loop cs
+                      (cons (list #\`) output))
+                (let (todo todo)
+                  todo)))))))
 
-(string-sequence "abc")
-
-("a" "aa" "ab" "aba" "abb" "abc")
+(string-sequence "abc") ; ("a" "aa" "ab" "aba" "abb" "abc")
