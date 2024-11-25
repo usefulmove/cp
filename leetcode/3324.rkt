2e5225b8a4828c89d4467b26e211b2cc571201a7
Duane Edmonds <duane.edmonds@gmail.com>
2024-11-25 14:54:43 -0800

    ...

--- leetcode/3324.rkt
+++ leetcode/3324.rkt
@@ -6,27 +6,38 @@
     (let loop ((cs (string->list target))
                (output '())
                (add-depth 1))
-      (cond ((empty? cs) (map ; convert character lists to strings
-                          (lambda (lst)
-                            (apply string (reverse lst)))
-                          (reverse (cdr output))))
-            ((empty? output) (loop cs
-                                   (cons (list #\a) output)
-                                   add-depth))
-            ((< (length (car output))
-                add-depth)            (loop cs
-                                            (cons (cons #\a (car output)) output)
-                                            (+ add-depth 1)))
-            ((equal? (car cs)
-                     (caar output)) (loop (cdr cs)
-                                               (cons (cons #\a (car output)) output)
-                                               (+ add-depth 1)))
-            (else (loop cs
-                        (cons (cons (next-c (caar output))
-                                    (cdar output))
-                              output)
-                        add-depth))))))
+      (cond
+        ;; base case - convert accumulated character lists to strings
+        ((empty? cs) (map
+                      (lambda (lst)
+                        (apply string (reverse lst)))
+                      (reverse (cdr output))))
 
+        ;; initialize output with '((#\a))
+        ((empty? output) (loop cs
+                               (cons (list #\a) output)
+                               add-depth))
+
+        ;; extend output to required depth
+        ((< (length (car output))
+            add-depth)            (loop cs
+                                        (cons (cons #\a (car output)) output)
+                                        (+ add-depth 1)))
+
+        ;; match target character - extend depth and reset to #\a
+        ((equal? (car cs)
+                 (caar output)) (loop (cdr cs)
+                                           (cons (cons #\a (car output)) output)
+                                           (+ add-depth 1)))
+
+        ;; increment leading character
+        (else (loop cs
+                    (cons (cons (next-c (caar output))
+                                (cdar output))
+                          output)
+                    add-depth))))))
+
+;; examples
 #;(string-sequence "ab") ; => ("a" "aa" "ab")
 #;(string-sequence "abc") ; => ("a" "aa" "ab" "aba" "abb" "abc")
 #;(string-sequence "aaaaaaaaaa")
