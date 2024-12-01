e530922cc5189f7c8fb9f4aff17b75a86ff1612d
Duane Edmonds <duane.edmonds@gmail.com>
2024-12-01 10:18:18 -0800

    ...

--- exercism/hamming.scm
+++ exercism/hamming.scm
@@ -1,5 +1,33 @@
 (import (rnrs))
 
+
+(define (hamming-distance strand-a strand-b)
+  (fold-left
+   (lambda (acc char-a char-b)
+     (+ acc
+        (if (equal? char-a char-b) 0 1)))
+   0
+   (string->list strand-a)
+   (string->list strand-b)))
+
+
+#;(define (hamming-distance strand-a strand-b)
+  (let loop ((chars-a (string->list strand-a))
+             (chars-b (string->list strand-b))
+             (result 0))
+    (cond ((and (null? chars-a)
+                (null? chars-b)) result)
+          ((or (null? chars-a)
+               (null? chars-b)) (error "strands are not the same length"))
+          ((equal? (car chars-a)
+                   (car chars-b)) (loop (cdr chars-a)
+                                        (cdr chars-b)
+                                        result))
+          (else (loop (cdr chars-a)
+                      (cdr chars-b)
+                      (+ result 1))))))
+
+
 #;(define (hamming-distance strand-a strand-b)
   (let ((zip (lambda (lst . lsts)
                (apply map list lst lsts))))
@@ -16,21 +44,5 @@
                  (string->list strand-b))))
           (else (error "strands are not the same length")))))
 
-
-(define (hamming-distance strand-a strand-b)
-  (let loop ((chars-a (string->list strand-a))
-             (chars-b (string->list strand-b))
-             (result 0))
-    (cond ((and (null? chars-a)
-                (null? chars-b)) result)
-          ((or (null? chars-a)
-               (null? chars-b)) (error "strands are not the same length"))
-          ((equal? (car chars-a)
-                   (car chars-b)) (loop (cdr chars-a)
-                                        (cdr chars-b)
-                                        result))
-          (else (loop (cdr chars-a)
-                      (cdr chars-b)
-                      (+ result 1))))))
 
 (hamming-distance "GAGCCTACTAACGGGAT" "CATCGTAATGACGGCCT")
