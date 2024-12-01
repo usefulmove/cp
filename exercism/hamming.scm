20fe1de4680026e46db73cf25ed68f9a6a84f8cd
Duane Edmonds <duane.edmonds@gmail.com>
2024-12-01 09:45:53 -0800

    ...

--- exercism/hamming.scm
+++ exercism/hamming.scm
@@ -1,6 +1,6 @@
 (import (rnrs))
 
-(define (hamming-distance strand-a strand-b)
+#;(define (hamming-distance strand-a strand-b)
   (let ((zip (lambda (lst . lsts)
                (apply map list lst lsts))))
     (cond ((= (string-length strand-a)
@@ -15,3 +15,22 @@
             (zip (string->list strand-a)
                  (string->list strand-b))))
           (else (error "strands are not the same length")))))
+
+
+(define (hamming-distance strand-a strand-b)
+  (let loop ((chars-a (string->list strand-a))
+             (chars-b (string->list strand-b))
+             (result 0))
+    (cond ((and (null? chars-a)
+                (null? chars-b)) result)
+          ((or (null? chars-a)
+               (null? chars-b)) (error "strands are not the same length"))
+          ((equal? (car chars-a)
+                   (car chars-b)) (loop (cdr chars-a)
+                                        (cdr chars-b)
+                                        result))
+          (else (loop (cdr chars-a)
+                      (cdr chars-b)
+                      (+ result 1))))))
+
+(hamming-distance "GAGCCTACTAACGGGAT" "CATCGTAATGACGGCCT") ; => 7
