444f287fe0c5f3422d6f8c76c6586dcb9e9ff28d
Duane Edmonds <duane.edmonds@gmail.com>
2024-12-02 18:27:03 -0800

    ...

---
+++ leetcode/2109.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (add-spaces s spaces)
+  (let loop ((cs (string->list s))
+             (output '())
+             (index 0)
+             (ss spaces))
+    (if (empty? cs)
+        (list->string (reverse output))
+        (loop (cdr cs)
+              (if (and (not (empty? ss))
+                       (= index (car ss)))
+                  (append (list (car cs) #\space)
+                          output)
+                  (cons (car cs) output))
+              (+ index 1)
+              (if (and (not (empty? ss))
+                       (= index (car ss)))
+                  (cdr ss)
+                  ss)))))
+
+(add-spaces "LeetcodeHelpsMeLearn" '(8 13 15))
+(add-spaces "spaces" (range 7))
