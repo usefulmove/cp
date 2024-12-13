c0817119cc65498c39814a25acebe3049cd5ffc8
Duane Edmonds <duane.edmonds@gmail.com>
2024-12-12 19:44:43 -0800

    ...

---
+++ leetcode/2593.rkt
@@ -0,0 +1,60 @@
+#lang racket
+
+(define (enumerate lst . params)
+  (let ((index (if (not (null? params)) (car params) 0)))
+    (cond ((null? lst) '())
+          (else (cons (cons index
+                            (car lst))
+                      (enumerate (cdr lst) (+ index 1)))))))
+
+(define (find-score nums)
+  (letrec ((nums-pairs (let loop ((ns nums)
+                                  (output '()))
+                         (if (empty? ns)
+                             (reverse output)
+                             (loop (cdr ns)
+                                   (cons (cons (car ns) #f)
+                                         output)))))
+           (all-marked? (lambda (lst)
+                          (cond ((empty? lst) #t)
+                                ((cdar lst) (all-marked? (cdr lst)))
+                                (else #f)))))
+    (let loop ((nps nums-pairs)
+               (score 0))
+      (if (all-marked? nps)
+          score
+          (let* ((min-pair (let loop ((pairs nps)
+                                      (ind 0)
+                                      (min-so-far 10e6)
+                                      (min-so-far-index -1))
+                             (if (empty? pairs)
+                                 (cons min-so-far-index min-so-far)
+                                 (let ((num (caar pairs))
+                                       (marked (cdar pairs)))
+                                   (if (and (not marked)
+                                            (< num min-so-far))
+                                       (loop (cdr pairs)
+                                             (+ ind 1)
+                                             num
+                                             ind)
+                                       (loop (cdr pairs)
+                                             (+ ind 1)
+                                             min-so-far
+                                             min-so-far-index))))))
+                 (min-marked-index (car min-pair))
+                 (minimum (cdr min-pair))
+                 (updated-nps (map
+                               (lambda (enum-pair)
+                                 (let ((ind (car enum-pair))
+                                       (pair (cdr enum-pair)))
+                                   (if (or (= ind min-marked-index)
+                                           (= ind (+ min-marked-index 1))
+                                           (= ind (- min-marked-index 1)))
+                                       (cons (car pair) #t)
+                                       pair)))
+                               (enumerate nps))))
+            (loop updated-nps
+                  (+ score minimum)))))))
+
+(find-score '(2 1 3 4 5 2)) ; => 7
+(find-score '(2 3 5 1 3 2)) ; => 5
