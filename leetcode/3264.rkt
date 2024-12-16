b5cf39b2fe5b5fe8d91a3c3ad60aaaabc540f07e
Duane Edmonds <duane.edmonds@gmail.com>
2024-12-15 22:02:03 -0800

    ...

--- leetcode/3264.rkt
+++ leetcode/3264.rkt
@@ -1,22 +1,22 @@
 #lang racket
 
 (define (get-final-state nums k multiplier)
-  (let ((get-min-index (lambda (lst)
-                         (let loop ((ns lst)
-                                    (ind 0)
-                                    (min-index -1)
-                                    (min-so-far 999))
-                           (if (empty? ns)
-                               min-index
-                               (if (< (car ns) min-so-far)
-                                   (loop (cdr ns)
-                                         (+ ind 1)
-                                         ind
-                                         (car ns))
-                                   (loop (cdr ns)
-                                         (+ ind 1)
-                                         min-index
-                                         min-so-far))))))
+  (let ((get-min-ind (lambda (lst)
+                       (let loop ((ns lst)
+                                  (ind 0)
+                                  (min-ind 0)
+                                  (min-so-far 2e32))
+                         (if (empty? ns)
+                             min-ind
+                             (if (< (car ns) min-so-far)
+                                 (loop (cdr ns)
+                                       (+ ind 1)
+                                       ind
+                                       (car ns))
+                                 (loop (cdr ns)
+                                       (+ ind 1)
+                                       min-ind
+                                       min-so-far))))))
         (update-nums (lambda (ns ind)
                        (append (take ns ind)
                                (list (* multiplier (list-ref ns ind)))
@@ -25,8 +25,10 @@
                (i k))
       (if (= i 0)
           ns
-          (loop (update-nums ns (get-min-index ns))
+          (loop (update-nums ns (get-min-ind ns))
                 (- i 1))))))
 
 (get-final-state '(2 1 3 5 6) 5 2)
 (get-final-state '(1 2) 3 4)
+(get-final-state '(4) 5 5)
+(get-final-state '(5 25) 10 5)
