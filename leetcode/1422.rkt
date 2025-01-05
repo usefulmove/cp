d40df37bc75b3091add94d2ae983867774f5cff1
Duane Edmonds <duane.edmonds@gmail.com>
2025-01-04 19:05:45 -0800

    ...

--- leetcode/1422.rkt
+++ leetcode/1422.rkt
@@ -1,24 +1,17 @@
 #lang racket
 
 (define (max-score s)
-  (let ((chars (string->list s))
-        (left (lambda (lst n)
-                (take lst n)))
-        (right (lambda (lst n)
-                 (drop lst n)))
-        (zeros (lambda (lst)
-                 (count
-                  (lambda (c)
-                    (equal? c #\0))
-                  lst)))
-        (ones (lambda (lst)
-                (count
-                 (lambda (c)
-                   (equal? c #\1))
-                 lst))))
+  (let* ((chars (string->list s))
+         (left take)
+         (right (lambda (lst n) (drop lst n)))
+         (zeros (lambda (lst)
+                  (count (lambda (c) (equal? c #\0)) lst)))
+         (ones (lambda (lst)
+                 (count (lambda (c) (equal? c #\1)) lst)))
+         (len (length chars)))
     (let loop ((ind 1)
                (max-so-far 0))
-      (if (= ind (length chars))
+      (if (= ind len)
           max-so-far
           (let ((score (+ (zeros (left chars ind))
                           (ones (right chars ind)))))
