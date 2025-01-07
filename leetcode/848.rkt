2cd55c7809e45eaa930960baa74c449d98afee60
Duane Edmonds <duane.edmonds@gmail.com>
2025-01-07 02:28:37 -0800

    ...

--- leetcode/848.rkt
+++ leetcode/848.rkt
@@ -1,28 +1,52 @@
 #lang racket
 
 (define (shifting-letters s shifts)
-  (let ((shift-char (lambda (c shift)
-                      (integer->char (+ (char->integer #\a)
-                                        (remainder
-                                         (- (+ (char->integer c)
-                                               shift)
-                                            (char->integer #\a))
-                                         26))))))
+  (let* ((alpha-root (char->integer #\a))
+         (shift-char (lambda (c shift)
+                       (integer->char (+ (remainder
+                                          (- (+ (char->integer c)
+                                                shift)
+                                             alpha-root)
+                                          26)
+                                         alpha-root)))))
     (list->string
-     (map
-      (lambda (pair)
-        (let* ((ind (car pair))
-               (c (cdr pair)))
-          (shift-char c (apply + (drop shifts ind)))))
-      (let loop ((chars (string->list s))
-                 (index 0)
-                 (output '()))
-        (if (empty? chars)
-            (reverse output)
-            (loop (cdr chars)
-                  (+ index 1)
-                  (cons (cons index (car chars))
-                        output))))))))
+     (let loop ((cs (reverse (string->list s)))
+                (ss (reverse shifts))
+                (acc 0)
+                (output '()))
+       (if (empty? cs)
+           output
+           (let ((c (car cs))
+                 (shift (car ss)))
+             (loop (cdr cs)
+                   (cdr ss)
+                   (+ acc shift)
+                   (cons (shift-char c (+ shift acc)) output))))))))
+
+#;(define (shifting-letters s shifts)
+  (let* ((alpha-root (char->integer #\a))
+         (shift-char (lambda (c shift)
+                       (integer->char (+ (remainder
+                                          (- (+ (char->integer c)
+                                                shift)
+                                             alpha-root)
+                                          26)
+                                         alpha-root)))))
+  (list->string
+   (map
+    (lambda (pair)
+      (let* ((ind (car pair))
+             (c (cdr pair)))
+        (shift-char c (apply + (drop shifts ind)))))
+    (let loop ((chars (string->list s))
+               (index 0)
+               (output '()))
+      (if (empty? chars)
+          (reverse output)
+          (loop (cdr chars)
+                (+ index 1)
+                (cons (cons index (car chars))
+                      output))))))))
 
 (shifting-letters "abc" '(3 5 9)) ; => "rpl"
 (shifting-letters "aaa" '(1 2 3)) ; => "gfd"
