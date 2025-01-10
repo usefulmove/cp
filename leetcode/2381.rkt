8bf553f1682ddc3ba38f1604b6a0971fbda79ac3
Duane Edmonds <duane.edmonds@gmail.com>
2025-01-10 00:10:39 -0800

    ...

---
+++ leetcode/2381.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(define (shifting-letters s shifts)
+  (let* ((alpha-root (char->integer #\a))
+         (shift-char (lambda (c direction)
+                       (integer->char (+ (remainder
+                                          (- (+ (char->integer c)
+                                                (if (= 0 direction) -1 1))
+                                             alpha-root)
+                                          26)
+                                         alpha-root))))
+         (len (string-length s))
+         (set-value (lambda (cs-init index value)
+                      (append (take cs-init index)
+                              (list value)
+                              (drop cs-init (+ index 1)))))
+         (apply-shift (lambda (cs-init shift)
+                        (let ((start (first shift))
+                              (end (second shift))
+                              (dir (third shift)))
+                          (let loop ((ind 0)
+                                     (cs cs-init))
+                            (if (= ind len)
+                                cs
+                                (loop (+ ind 1)
+                                      (if (and (>= ind start)
+                                               (<= ind end))
+                                          (set-value cs ind (shift-char (list-ref cs ind) dir))
+                                          cs))))))))
+    (list->string (foldl
+                   (lambda (shift acc)
+                     (apply-shift acc shift))
+                   (string->list s)
+                   shifts))))
+
+(shifting-letters "abc" '((0 1 0) (1 2 1) (0 2 1))) ; => "ace"
+(shifting-letters "dztz" '((0 0 0) (1 1 1))) ; => "catz"
