9eb5e926dd8a9f535b67e45c840082aea20b377c
Duane Edmonds <duane.edmonds@gmail.com>
2025-01-16 12:49:49 -0800

    ...

--- leetcode/1769.rkt
+++ leetcode/1769.rkt
@@ -1,20 +1,19 @@
 #lang racket
 
 (define (min-operations boxes)
-  (let* ((len (string-length boxes))
-         (bs (string->list boxes))
-         (get-ops (lambda (i)
-                       (foldl
-                        (lambda (j acc)
-                          (+ acc (if (or (= i j)
-                                         (equal? (list-ref bs j) #\0))
-                                     0
-                                     (abs (- i j)))))
-                        0
-                        (range len)))))
+  (let* ((bs (string->list boxes))
+         (indicies (range (string-length boxes))))
     (map
-     (lambda (index) (get-ops index))
-     (range len))))
+     (lambda (i)
+       (foldl
+        (lambda (j acc)
+          (+ acc (if (or (= i j)
+                         (equal? (list-ref bs j) #\0))
+                     0
+                     (abs (- i j)))))
+        0
+        indicies))
+     indicies)))
 
 (min-operations "110") ; => '(1 1 3)
 (min-operations "001011") ; => '(11 8 5 4 3 4)
