92993d146748a055aeee2b5cd43998eb5769cea0
Duane Edmonds <duane.edmonds@gmail.com>
2025-01-15 20:33:58 -0800

    ...

--- leetcode/2425.rkt
+++ leetcode/2425.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define (xor-all-nums nums1 nums2)
+#;(define (xor-all-nums nums1 nums2)
   (foldl
    (lambda (num-a acc)
      (bitwise-xor acc (foldl
@@ -10,6 +10,15 @@
                        nums2)))
    0
    nums1))
+
+(define (xor-all-nums nums1 nums2)
+  (let ((a (if (even? (length nums1))
+               0
+               (apply bitwise-xor nums2)))
+        (b (if (even? (length nums2))
+               0
+               (apply bitwise-xor nums1))))
+    (bitwise-xor a b)))
 
 (xor-all-nums '(2 1 3) '(10 2 5 0)) ; => 13
 (xor-all-nums '(1 2) '(3 4)) ; => 0
