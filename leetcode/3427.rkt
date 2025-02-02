bc3a359d731c11da0207c8594937e418ed9ee074
Duane Edmonds <duane.edmonds@gmail.com>
2025-02-02 14:43:34 -0800

    ...

--- leetcode/3427.rkt
+++ leetcode/3427.rkt
@@ -2,15 +2,14 @@
 
 (define (subarray-sum nums)
   (let ((sublist (lambda (lst i j)
-                   (drop (take lst j) i)))
-        (len (length nums)))
+                   (drop (take lst j) i))))
     (foldl
      (lambda (i acc)
        (let* ((start (max 0 (- i (list-ref nums i))))
               (sub (sublist nums start (+ i 1))))
          (+ acc (apply + sub))))
      0
-     (range len))))
+     (range (length nums)))))
 
 (subarray-sum '(2 3 1))
 (subarray-sum '(3 1 1 2))
