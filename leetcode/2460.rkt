57b0dd70742305a6e2f806b812888e7388219196
Duane Edmonds <duane.edmonds@gmail.com>
2025-03-01 10:02:18 -0800

    ...

--- leetcode/2460.rkt
+++ leetcode/2460.rkt
@@ -12,12 +12,16 @@
                                                       (cons 0 (cons (* (car ns) 2)
                                                                     output))))
                                  (else (loop (cdr ns)
-                                             (cons (car ns) output))))))
-         (nonzero-vals (filter
-                        (lambda (a)
-                          (not (zero? a)))
-                        processed-nums))
-         (zeros (filter zero? processed-nums)))
-    (append nonzero-vals zeros)))
+                                             (cons (car ns) output)))))))
+    (let loop ((pns processed-nums)
+               (vals '())
+               (zeros '()))
+      (cond ((empty? pns) (append (reverse vals) zeros))
+            ((zero? (car pns)) (loop (cdr pns)
+                                     vals
+                                     (cons 0 zeros)))
+            (else (loop (cdr pns)
+                        (cons (car pns) vals)
+                        zeros))))))
 
 (apply-operations '(1 2 2 1 1 0))
