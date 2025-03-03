59a746fc713bfe8c857cd8106453fb907ac2bd64
Duane Edmonds <duane.edmonds@gmail.com>
2025-03-02 17:15:30 -0800

    ...

---
+++ leetcode/2149.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+(define (rearrange-array nums)
+  (let loop ((ns nums)
+             (pos-sign #t)
+             (negs '())
+             (poss '())
+             (output '()))
+    (print ns)
+    (print pos-sign)
+    (print negs)
+    (print poss)
+    (println output)
+    (cond ((and (empty? ns)
+                (empty? negs)
+                (empty? poss)) (reverse output))
+          ((and pos-sign
+                (not (empty? poss))) (loop ns
+                                           (not pos-sign)
+                                           negs
+                                           (take poss (- (length poss) 1))
+                                           (cons (last poss) output)))
+          ((and pos-sign
+                (not (empty? ns))) (if (positive? (car ns))
+                                       (loop (cdr ns)
+                                             (not pos-sign)
+                                             negs
+                                             poss
+                                             (cons (car ns) output))
+                                       (loop (cdr ns)
+                                             pos-sign
+                                             (cons (car ns) negs)
+                                             poss
+                                             output)))
+          ((not (empty? negs)) (loop ns
+                                     (not pos-sign)
+                                     (take negs (- (length negs) 1))
+                                     poss
+                                     (cons (last negs) output)))
+          ((not (empty? ns)) (if (negative? (car ns))
+                                 (loop (cdr ns)
+                                       (not pos-sign)
+                                       negs
+                                       poss
+                                       (cons (car ns) output))
+                                 (loop (cdr ns)
+                                       pos-sign
+                                       negs
+                                       (cons (car ns) poss)
+                                       output))))))
+
+(rearrange-array '(3 1 -2 -5 2 -4)) ; => '(3 -2 1 -5 2 -4)
+(rearrange-array '(-1 1)) ; => '(1 -1)
