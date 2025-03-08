2c82c5137cb634ee66abca98315f02ac0b3c2fde
Duane Edmonds <duane.edmonds@gmail.com>
2025-03-07 18:46:45 -0800

    ...

--- leetcode/2379.rkt
+++ leetcode/2379.rkt
@@ -1,36 +1,20 @@
 #lang racket
 
-(define (minimum-recolors blocks k)
-  (let* ((black #\B)
-         (white #\W)
-         (count-recolors (lambda (bs)
+(define/contract (minimum-recolors blocks k)
+  (-> string? exact-integer? exact-integer?)
+  (let* ((count-recolors (lambda (bs)
                            (count
                             (lambda (block)
-                              (equal? white block))
+                              (equal? #\W block))
                             (take bs k)))))
     (let loop ((bs (string->list blocks))
                (recolors 2e32))
       (cond ((< (length bs) k) recolors)
-            ((< (count-recolors bs) recolors) (loop (cdr bs)
-                                                    (count-recolors bs)))
-            (else
-             (loop (cdr bs)
-                     (let* ((black #\B)
-         (white #\W)
-         (count-recolors (lambda (bs)
-                           (count
-                            (lambda (block)
-                              (equal? white block))
-                            (take bs k)))))
-    (let loop ((bs (string->list blocks))
-               (recolors 2e32))
-      (cond ((< (length bs) k) recolors)
-            ((< (count-recolors bs) recolors) (loop (cdr bs)
-                                                    (count-recolors bs)))
-            (else
-             (loop (cdr bs)
-                   recolors))))))
-recolors))))))
+            (else (let ((comp-recolors (count-recolors bs)))
+                    (loop (cdr bs)
+                          (if (< comp-recolors recolors)
+                              comp-recolors
+                              recolors))))))))
 
 (minimum-recolors "WBBWWBBWBW" 7)
 (minimum-recolors "WBWBBBW" 2)
