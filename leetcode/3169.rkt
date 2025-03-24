c3ecd300fd45c22430abf1b4c75ec5e4aca61e55
Duane Edmonds <duane.edmonds@gmail.com>
2025-03-23 18:01:12 -0700

    ...

--- leetcode/3169.rkt
+++ leetcode/3169.rkt
@@ -1,23 +1,40 @@
 #lang racket
 
 (define (count-days days meetings)
-  (count false? (foldl
-                 (lambda (meeting days-unavailable)
-                   (let ((start (first meeting))
-                         (end (second meeting)))
-                     (let loop ((days days-unavailable)
-                                (index 0)
-                                (output '()))
-                       (cond ((empty? days) (reverse output))
-                             ((<= start (+ index 1) end)
-                              (loop (cdr days)
-                                    (+ index 1)
-                                    (cons #t output)))
-                             (else (loop (cdr days)
-                                         (+ index 1)
-                                         (cons (car days) output)))))))
-                 (make-list days #f) ; days unavailable
-                 meetings)))
+  (foldl
+   (lambda (day available-days)
+     (+ available-days
+        (if (foldl
+             (lambda (meeting is-available)
+               (let ((start (first meeting))
+                     (end (second meeting)))
+                 (and is-available
+                      (not (<= start (add1 day) end)))))
+             #t
+             meetings)
+            1
+            0)))
+   0
+   (range days)))
+
+#;(define (count-days days meetings)
+    (count false? (foldl
+              (lambda (meeting days-unavailable)
+                (let ((start (first meeting))
+                      (end (second meeting)))
+                  (let loop ((days days-unavailable)
+                            (index 0)
+                            (output '()))
+                    (cond ((empty? days) (reverse output))
+                          ((<= start (+ index 1) end)
+                          (loop (cdr days)
+                                (+ index 1)
+                                (cons #t output)))
+                          (else (loop (cdr days)
+                                      (+ index 1)
+                                      (cons (car days) output)))))))
+              (make-list days #f) ; days unavailable
+              meetings)))
 
 (count-days 10 '((5 7) (1 3) (9 10))) ; => 2
 (count-days 5 '((2 4) (1 3))) ; => 1
