6086b9c4ed6796e8808ff6f51b8df18a7f71af71
Duane Edmonds <duane.edmonds@gmail.com>
2025-04-22 18:51:33 -0700

    ...

--- leetcode/1399.rkt
+++ leetcode/1399.rkt
@@ -1,33 +1,28 @@
 #lang racket
 
 (define (count-largest-group n)
-  (letrec ((count-largest-groups (lambda (groups)
-                                   (let ((group-lengths
-                                          (map length (hash-values groups))))
-                                     (let loop ((lens group-lengths)
-                                                (longest 0)
-                                                (longest-count 0))
-                                       (cond ((empty? lens) longest-count)
-                                             ((> (car lens) longest)
-                                              (loop (cdr lens)
-                                                    (car lens)
-                                                    1))
-                                             ((= (car lens) longest)
-                                              (loop (cdr lens)
-                                                    longest
-                                                    (add1 longest-count)))
-                                             (else (loop (cdr lens)
-                                                         longest
-                                                         longest-count)))))))
-           (sum-digits (lambda (a)
-                         (if (< a 10)
-                             a
-                             (+ (modulo a 10)
-                                (sum-digits (quotient a 10)))))))
+  (letrec ((count-largest (lambda (lengths)
+                            (let loop ((lens lengths)
+                                       (longest 0)
+                                       (cnt 0))
+                              (cond ((empty? lens) cnt)
+                                    ((> (car lens) longest) (loop (cdr lens)
+                                                                  (car lens)
+                                                                  1))
+                                    ((= (car lens) longest) (loop (cdr lens)
+                                                                  longest
+                                                                  (add1 cnt)))
+                                    (else (loop (cdr lens)
+                                                longest
+                                                cnt))))))
+           (sum-digits (lambda (a) (if (< a 10)
+                                       a
+                                       (+ (modulo a 10)
+                                          (sum-digits (quotient a 10)))))))
     (let loop ((num 1)
                (groups (hash)))
       (if (> num n)
-          (count-largest-groups groups)
+          (count-largest (map length (hash-values groups)))
           (let* ((sum (sum-digits num))
                  (nums (hash-ref groups sum #f)))
             (if nums
