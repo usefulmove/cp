9a6de9a4bace2161ba3568de2c3d64a6790df6f1
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-02 21:02:49 -0700

    ...

--- leetcode/1695.rkt
+++ leetcode/1695.rkt
@@ -1,26 +1,34 @@
 #lang racket
 
 (define (maximum-unique-subarray nums)
-  (let ((sublist (lambda (lst start count)
-                   (take (drop lst start) count)))
+  (let ((sublist (lambda (lst count)
+                   (if (> count (length lst))
+                       lst
+                       (take lst count))))
         (all-unique? (lambda (lst)
-                       (= (length lst)
-                          (length (remove-duplicates lst))))))
+                       (let loop ((es lst)
+                                  (seen '()))
+                         (cond ((empty? es) #t)
+                               ((member (car es) seen) #f)
+                               (else (loop (cdr es)
+                                           (cons (car es) seen))))))))
     (let recur ((ns nums)
                 (cnt 1)
                 (max-so-far 0))
-      (cond ((empty? ns) max-so-far)
-            ((or (> cnt (length ns))
-                 (not (all-unique? (sublist ns 0 cnt)))) (recur (cdr ns)
-                                                                1 ; reset
-                                                                max-so-far))
-            ((> (apply + (sublist ns 0 cnt)) max-so-far)
-             (recur ns
-                    (add1 cnt)
-                    (apply + (sublist ns 0 cnt))))
-            (else (recur ns
-                         (add1 cnt)
-                         max-so-far))))))
+      (if (empty? ns)
+          max-so-far
+          (let* ((sublst (sublist ns cnt))
+                 (sum (apply + sublst)))
+            (cond ((or (> cnt (length ns))
+                       (not (all-unique? sublst))) (recur (cdr ns)
+                                                          1 ; reset
+                                                          max-so-far))
+                  ((> sum max-so-far) (recur ns
+                                             (add1 cnt)
+                                             sum))
+                  (else (recur ns
+                               (add1 cnt)
+                               max-so-far))))))))
 
 (maximum-unique-subarray '(4 2 4 5 6)) ; 17
 (maximum-unique-subarray '(5 2 1 2 5 2 1 2 5)) ; 8
