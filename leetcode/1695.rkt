d84720c9302c31a91251b0e05c8762959e585510
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-02 21:59:52 -0700

    ...

--- leetcode/1695.rkt
+++ leetcode/1695.rkt
@@ -1,6 +1,27 @@
 #lang racket
 
 (define (maximum-unique-subarray nums)
+  (let ((all-unique? (lambda (lst)
+                       (= (length lst)
+                          (length (remove-duplicates lst))))))
+    (let recur ((ns nums)
+                (cnt 1)
+                (max-so-far 0))
+      (if (empty? ns)
+          max-so-far
+          (cond ((> cnt (length ns)) (recur (cdr ns)
+                                            1 ; reset
+                                            (max (apply + ns)
+                                                 max-so-far)))
+                ((all-unique? (take ns cnt)) (recur ns
+                                                    (add1 cnt)
+                                                    max-so-far))
+                (else (recur (cdr ns)
+                             1 ; reset
+                             (max max-so-far
+                                  (apply + (take ns (sub1 cnt)))))))))))
+
+#;(define (maximum-unique-subarray nums)
   (let ((sublist (lambda (lst count)
                    (if (> count (length lst))
                        lst
