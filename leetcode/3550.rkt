2257a0d5f919c1bb6d869b4a7c4bc1a1aa5296ac
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-11 20:25:37 -0700

    ...

--- leetcode/3550.rkt
+++ leetcode/3550.rkt
@@ -2,18 +2,13 @@
 
 (define (smallest-index nums)
   (letrec ((digit-sum (lambda (n)
-                        (if (< n 10)
-                            n
-                            (+ (remainder n 10)
-                               (digit-sum (quotient n 10)))))))
-    (let recur ((ns nums)
-                (index 0))
-      (if (empty? ns)
-          -1
-          (if (= (digit-sum (car ns)) index)
-              index
-              (recur (cdr ns)
-                     (+ index 1)))))))
+                        (cond ((< n 10) n)
+                              (else (+ (remainder n 10)
+                                       (digit-sum (quotient n 10))))))))
+    (let recur ((ns nums) (index 0))
+      (cond ((empty? ns) -1)
+            ((= (digit-sum (car ns)) index) index)
+            (else (recur (cdr ns) (+ index 1)))))))
 
 (smallest-index '(1 3 2)) ; => 2
 (smallest-index '(1 10 11)) ; => 1
