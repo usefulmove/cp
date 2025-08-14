7cc0e730e007090c3f42a996537524da90fbdd2e
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-13 19:23:49 -0700

    ...

---
+++ leetcode/1903.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+#;(require srfi/13)
+#;(define (largest-odd-number num)
+  (let* ((cs (string->list num))
+         (odds (indexes-where (map char->integer cs) odd?)))
+    (if (empty? odds)
+        ""
+        (string-take cs (add1 (last odds))))))
+
+(define (largest-odd-number num)
+  (let loop ((cs (reverse (string->list num))))
+    (cond ((empty? cs) "")
+          ((odd? (char->integer (car cs))) (list->string (reverse cs)))
+          (else (loop (cdr cs))))))
+
+(largest-odd-number "52")
+(largest-odd-number "4206")
+(largest-odd-number "35427")
