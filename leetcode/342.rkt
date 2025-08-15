864eb60ebb8e2c42806806b4ca86f29e40b87449
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-14 17:24:40 -0700

    ...

--- leetcode/342.rkt
+++ leetcode/342.rkt
@@ -1,11 +1,10 @@
 #lang racket
 
 (define (is-power-of-four n)
-  (if (<= n 0)
-      #f
-      (< (abs (- (log n 4)
-                 (round (log n 4))))
-         0.0000000000001)))
+  (and (not (<= n 0))
+       (< (abs (- (log n 4)
+                  (round (log n 4))))
+          0.0000000000001)))
 
 (is-power-of-four 16) ; => #t
 (is-power-of-four 5) ; => #f
