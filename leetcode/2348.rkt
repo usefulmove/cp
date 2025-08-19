ccd658f197321032beda240e01a5a7d72bfdc180
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-18 19:17:13 -0700

    ...

--- leetcode/2348.rkt
+++ leetcode/2348.rkt
@@ -1,18 +1,48 @@
 #lang racket
 
 (define (zero-filled-subarray nums)
-  (let loop ((xs nums) (cnt 0) (groups '()))
-    (cond ((empty? xs) (foldl
-                        (lambda (x acc)
-                          (+ acc (/ (* x (+ x 1)) 2)))
-                        0
-                        (cons cnt groups)))
-          ((zero? (car xs)) (loop (cdr xs)
+  (let ((tng (lambda (a) ; triangular number
+               (/ (* a (+ a 1)) 2))))
+    (let loop ((ns nums) (cnt 0) (acc 0))
+      (cond ((empty? ns) (+ acc (tng cnt)))
+            ((zero? (car ns)) (loop (cdr ns)
+                                    (+ cnt 1)
+                                    acc))
+            (else (loop (cdr ns)
+                        0 ; reset
+                        (+ acc (tng cnt))))))))
+
+#;(define (zero-filled-subarray nums)
+  (let loop ((ns nums) (cnt 0) (counts '()))
+    (cond ((empty? ns) (let recur ((cnts (cons cnt counts))
+                                   (out 0))
+                         (if (empty? cnts)
+                             out
+                             (recur (cdr cnts)
+                                    (+ out
+                                       (/ (* (car cnts)
+                                             (+ (car cnts) 1))
+                                          2))))))
+          ((zero? (car ns)) (loop (cdr ns)
                                   (+ cnt 1)
-                                  groups))
-          (else (loop (cdr xs)
+                                  counts))
+          (else (loop (cdr ns)
                       0 ; reset
-                      (cons cnt groups))))))
+                      (cons cnt counts))))))
+
+#;(define (zero-filled-subarray nums)
+  (let loop ((ns nums) (cnt 0) (counts '()))
+    (cond ((empty? ns) (foldl
+                        (lambda (c acc)
+                          (+ acc (/ (* c (+ c 1)) 2)))
+                        0
+                        (cons cnt counts)))
+          ((zero? (car ns)) (loop (cdr ns)
+                                  (+ cnt 1)
+                                  counts))
+          (else (loop (cdr ns)
+                      0 ; reset
+                      (cons cnt counts))))))
 
 (zero-filled-subarray '(1 3 0 0 2 0 0 4)) ; => 6
 (zero-filled-subarray '(0 0 0 2 0 0)) ; => 9
