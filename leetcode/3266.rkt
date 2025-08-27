a84aa5a3bab56c1fa6b529953eb134b6b6692413
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-26 18:03:25 -0700

    ...

--- leetcode/3266.rkt
+++ leetcode/3266.rkt
@@ -1,20 +1,18 @@
 #lang racket
 
 (define (check-divisibility n)
-  (let ((dsum (let loop ((a n)
-                         (sum 0))
+  (let ((digit-sum (let loop ((a n) (sum 0))
                 (if (zero? a)
                     sum
                     (loop (quotient a 10)
                           (+ sum (remainder a 10))))))
-        (dprod (let loop ((b n)
-                          (prod 1))
+        (digit-prod (let loop ((b n) (prod 1))
                  (if (zero? b)
                      prod
                      (loop (quotient b 10)
                            (* prod (remainder  b 10)))))))
-    (zero? (remainder n (+ dsum dprod)))))
+    (zero? (remainder n (+ digit-sum digit-prod)))))
 
 (check-divisibility 99) ; => #t
 (check-divisibility 23) ; => #f
-(check-divisibility 8)
+(check-divisibility 8) ; => #f
