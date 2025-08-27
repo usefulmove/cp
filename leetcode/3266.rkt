b68597a87ef6a7fab55994f7a2a36b556295e6ff
Duane Edmonds <duane.edmonds@gmail.com>
2025-08-26 18:08:21 -0700

    ...

--- leetcode/3266.rkt
+++ leetcode/3266.rkt
@@ -2,15 +2,15 @@
 
 (define (check-divisibility n)
   (let ((digit-sum (let loop ((a n) (sum 0))
-                (if (zero? a)
-                    sum
-                    (loop (quotient a 10)
-                          (+ sum (remainder a 10))))))
+          (if (zero? a)
+              sum
+              (loop (quotient a 10)
+                    (+ sum (remainder a 10))))))
         (digit-prod (let loop ((b n) (prod 1))
-                 (if (zero? b)
-                     prod
-                     (loop (quotient b 10)
-                           (* prod (remainder  b 10)))))))
+          (if (zero? b)
+              prod
+              (loop (quotient b 10)
+                    (* prod (remainder  b 10)))))))
     (zero? (remainder n (+ digit-sum digit-prod)))))
 
 (check-divisibility 99) ; => #t
