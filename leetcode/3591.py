27e0bb44df84fa503fd4056c8d9a075b8f108dd3
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-05 22:18:00 -0700

    ...

--- leetcode/3591.py
+++ leetcode/3591.py
@@ -2,14 +2,10 @@ from collections import Counter
 
 class Solution:
     primes = [
-        2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-        31, 37, 41, 43, 47, 53, 59, 61, 67,
-        71, 73, 79, 83, 89, 97
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
+        59, 61, 67, 71, 73, 79, 83, 89, 97
     ]
-
-    def isPrime(self, n: int) -> bool:
-        return n in primes
 
     def checkPrimeFrequency(self, nums: List[int]) -> bool:
         counts = Counter(nums)
-        return any(map(self.isPrime, counts.values()))
+        return any(map(lambda n: n in self.primes, counts.values()))
