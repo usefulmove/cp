d56b9c218fc2968f7f7bec18410423ead70e4635
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-12 19:47:40 -0700

    ...

--- leetcode/3541.rkt
+++ leetcode/3541.rkt
@@ -3,28 +3,28 @@
 (define (max-freq-sum s)
   (let* ((chars (string->list s))
          (vowel? (lambda (c) (member c '(#\a #\e #\i #\o #\u))))
-         (counter (lambda (lst)
-                    (let loop ((cs chars)
-                               (out '())) ; assoc list
-                      (if (empty? cs)
-                          (reverse out)
-                          (let* ((c (car cs))
-                                 (ind (index-of (map car out) c)))
-                            (loop (cdr cs)
-                                  (if ind
-                                      (append (take out ind)
-                                              (list (cons c (add1 (cdr (list-ref out ind)))))
-                                              (drop out (add1 ind)))
-                                      (cons (cons c 1) out))))))))
-         (counts (counter chars)))
+         (counts (let loop ((cs chars)
+                            (cnts '())) ; assoc list
+                   (if (empty? cs)
+                       (reverse cnts)
+                       (let* ((c (car cs))
+                              (ind (index-of (map car cnts) c)))
+                         (loop (cdr cs)
+                               (if ind
+                                   (append (take cnts ind)
+                                           (let ((c-cnt (cdr
+                                                         (list-ref cnts ind))))
+                                             (list (cons c (+ c-cnt 1))))
+                                           (drop cnts (+ ind 1)))
+                                   (cons (cons c 1) cnts))))))))
     (let loop ((cnts counts)
                (max-vowel 0)
                (max-consonant 0))
       (cond ((empty? cnts) (+ max-vowel max-consonant))
-            ((vowel? (caar cnts)) (loop (cdr cnts)
+            ((vowel? (caar cnts)) (loop (cdr cnts) ; vowel
                                         (max max-vowel (cdar cnts))
                                         max-consonant))
-            (else (loop (cdr cnts)
+            (else (loop (cdr cnts) ; consonant
                         max-vowel
                         (max max-consonant (cdar cnts))))))))
 
