75b7325f1d11ac0aaaa978e92ba36e6e443d8293
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-14 17:49:40 -0700

    ...

--- leetcode/1935.rkt
+++ leetcode/1935.rkt
@@ -3,12 +3,11 @@
 (define (can-be-typed-words text brokenLetters)
   (let ((broken-letters (string->list brokenLetters)))
     (count
-     (lambda (word)
-       (let loop ((cs (string->list word)))
-         (if (empty? cs)
-             #t
-             (and (not (member (car cs) broken-letters))
-                  (loop (cdr cs))))))
+     (lambda (word) (let loop ((cs (string->list word)))
+                      (if (empty? cs)
+                          #t
+                          (and (not (member (car cs) broken-letters))
+                               (loop (cdr cs))))))
      (string-split text))))
 
 (can-be-typed-words "hello world" "ad") ; => 1
