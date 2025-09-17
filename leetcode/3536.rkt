c3bdd6a35d923c3b386d1502d19d61ecab4928e6
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-16 17:45:32 -0700

    ...

--- leetcode/3536.rkt
+++ leetcode/3536.rkt
@@ -1,6 +1,19 @@
 #lang racket
 
 (define (max-product n)
+  (let loop ((num n)
+             (largest '(0 0))) ; '(a b) where a => b
+    (if (zero? num)
+        (apply * largest)
+        (let ((digit (remainder num 10))
+              (a (car largest))
+              (b (cadr largest)))
+          (loop (quotient num 10)
+                (cond ((>= digit a) (list digit a))
+                      ((>= digit b) (list a digit))
+                      (else (list a b))))))))
+
+#;(define (max-product n)
   (let ((digits (sort
                  (let loop ((num n)
                             (digs '()))
