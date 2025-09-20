ddf19da4b22fa069c615ec200720b543cb4804af
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-19 18:03:41 -0700

    ...

--- leetcode/28.rkt
+++ leetcode/28.rkt
@@ -1,14 +1,13 @@
 #lang racket
 
 (define (str-str haystack needle)
-  (let* ((haystack-cs (string->list haystack))
-         (needle-cs (string->list needle))
-         (len (length needle-cs)))
-    (let loop ((cs haystack-cs)
+  (let* ((n-haystack (string-length haystack))
+         (n-needle (string-length needle))
+         (needle-cs (string->list needle)))
+    (let loop ((cs (string->list haystack))
                (index 0))
-      (cond ((or (empty? cs)
-                 (< (length cs) len)) -1)
-            ((equal? (take cs len) needle-cs) index)
+      (cond ((< (- n-haystack index) n-needle) -1)
+            ((equal? (take cs n-needle) needle-cs) index)
             (else (loop (cdr cs)
                         (+ index 1)))))))
 
