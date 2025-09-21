4a85f324eff2b3cc7e44da12d4ef7576d064cd99
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-21 16:44:55 -0700

    ...

--- leetcode/3095.py
+++ leetcode/3095.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums) + 1):
                 sub = nums[i:j]
-                if reduce(lambda a, b: a | b, sub) >= k:
-                    if j - i < shortest:
+                if j - i < shortest:
+                    if reduce(lambda a, b: a | b, sub) >= k:
                         shortest = j - i
 
         return shortest if shortest != 51 else -1
