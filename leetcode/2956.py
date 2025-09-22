ba88c674ab3cdcb9dff939e36aa72c020bc2f32a
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-21 18:34:13 -0700

    ...

--- leetcode/2956.py
+++ leetcode/2956.py
@@ -2,4 +2,5 @@ class Solution:
     def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
         onesInTwo = sum(1 for n1 in nums1 if n1 in nums2)
         twosInOne = sum(1 for n2 in nums2 if n2 in nums1)
+
         return [onesInTwo, twosInOne]
