bafa858caddf2a170a8106f24a225bcd8d469370
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-21 18:21:49 -0700

    ...

--- leetcode/3304.rkt
+++ leetcode/3304.rkt
@@ -1,15 +1,13 @@
 #lang racket
 
 (define (kth-character k)
-  (let ((next (lambda (lst)
-                (map
-                 (lambda (o)
-                   (remainder (+ o 1) 26))
-                 lst))))
-    (let loop ((cs '(1)))
+  (let ((calc-nexts (lambda (lst) (map
+                                   (lambda (o) (remainder (+ o 1) 26))
+                                   lst))))
+    (let loop ((cs '(1))) ; a=1, b=2, ...
       (if (>= (length cs) k)
-          (integer->char (+ 96 (list-ref cs (- k 1))))
-          (loop (append cs (next cs)))))))
+          (integer->char (+ 96 (list-ref cs (- k 1)))) ; return character
+          (loop (append cs (calc-nexts cs)))))))
 
 (kth-character 5) ; => #\b
 (kth-character 10) ; => #\c
