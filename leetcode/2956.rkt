b31be305c8b14cf062c6762f11cb6ea1707211a5
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-21 18:27:08 -0700

    ...

--- leetcode/2956.rkt
+++ leetcode/2956.rkt
@@ -1,12 +1,8 @@
 #lang racket
 
 (define (find-intersection-values nums1 nums2)
-  (let ((ones-in-two (count
-                      (lambda (one) (member one nums2))
-                      nums1))
-        (twos-in-one (count
-                      (lambda (two) (member two nums1))
-                      nums2)))
+  (let ((ones-in-two (count (lambda (one) (member one nums2)) nums1))
+        (twos-in-one (count (lambda (two) (member two nums1)) nums2)))
     (list ones-in-two twos-in-one)))
 
 (find-intersection-values '(2 3 2) '(1 2)) ; => '(2 1)
