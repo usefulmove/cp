7f0f91ccdfad846a1db267d8660da3094c3c4657
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-26 03:08:39 -0700

    ...

--- leetcode/3232.rkt
+++ leetcode/3232.rkt
@@ -1,6 +1,14 @@
 #lang racket
 
 (define (can-alice-win nums)
+  (not (zero? (foldl
+               (lambda (n acc) (+ acc (if (< n 10)
+                                          n
+                                          (- n))))
+               0
+               nums))))
+
+#;(define (can-alice-win nums)
   (let loop ((ns nums)
              (single-sum 0)
              (double-sum 0))
