78867bdc95babafaee7cf42a4c3609d826df411b
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-26 01:52:02 -0700

    ...

---
+++ leetcode/2833.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (furthest-distance-from-origin moves)
+  (let loop ((mvs (string->list moves))
+             (pos 0))
+    (cond ((empty? mvs) (abs pos))
+          ((equal? #\L (car mvs)) (loop (cdr mvs)
+                                        (- pos 1)))
+          ((equal? #\R (car mvs)) (loop (cdr mvs)
+                                        (+ pos 1)))
+          (else (max (loop (cons #\L (cdr mvs))
+                           pos)
+                     (loop (cons #\R (cdr mvs))
+                           pos))))))
+
+(furthest-distance-from-origin "L_RL__R")
+(furthest-distance-from-origin "_R__LL_")
+(furthest-distance-from-origin "_______")
