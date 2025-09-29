838df873a199ceef27dc70a6112966aba0bda88f
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-28 20:08:24 -0700

    ...

--- leetcode/3697.rkt
+++ leetcode/3697.rkt
@@ -1,6 +1,24 @@
 #lang racket
 
+#|
+Iterate through each digit `(remainder x 10)`, keeping track of the digit index, and calculate the base-10 contribution if the digit is non-zero.
+|#
+
 (define (decimal-representation n)
+  (let loop ((res n)
+             (ind 0) ; digit position index
+             (out '())) ; base-10 components
+    (if (zero? res)
+        out ; return output
+        (let* ((digit (remainder res 10))
+               (residual (quotient res 10)))
+          (if (zero? digit)
+              (loop residual (+ ind 1) out) ; ignore digit
+              (loop residual
+                    (+ ind 1)
+                    (cons (* digit (expt 10 ind)) out)))))))
+
+#;(define (decimal-representation n)
   (let ((digits (let loop ((a n)
                            (out '()))
                   (if (zero? a)
