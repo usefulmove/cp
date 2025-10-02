0de08964f9495fa7e5721d36bd52506bc31850c3
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-01 18:20:42 -0700

    ...

--- leetcode/1318.py
+++ leetcode/1318.py
@@ -2,17 +2,17 @@ class Solution:
     def minFlops(self, a: int, b: int, c: int) -> int:
         a_bits = f"{a:0>32b}"
         b_bits = f"{b:0>32b}"
-        aorb_bits = f"{a|b:0>32b}"
         c_bits = f"{c:0>32b}"
+        aorb_bits = f"{a|b:0>32b}"
 
-        def f(a, b, c, o):
-            if o == c:
-                return 0
-            elif o == '0' or a == '0' or b == '0':
-                return 1
+        def f(acc, tup):
+            a, b, c, aorb = tup
+
+            if aorb == c:
+                return acc
+            elif aorb == '0' or a == '0' or b == '0':
+                return acc + 1
             else:
-                return 2
+                return acc + 2
 
-        m = list(map(f, a_bits, b_bits, c_bits, aorb_bits))
-
-        return sum(m)
+        return reduce(f, zip(a_bits, b_bits, c_bits, aorb_bits), 0)
