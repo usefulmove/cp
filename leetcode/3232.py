fa9305bca4e7081ef79c5a422f5685d341379f2d
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-02 17:08:10 -0700

    ...

--- leetcode/3232.py
+++ leetcode/3232.py
@@ -1,6 +1,4 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
-        def f(acc, n):
-            return acc + (n if n < 10 else -n)
-
+        f = lambda acc, n: acc + (n if n < 10 else -n)
         return reduce(f, nums, 0) != 0
