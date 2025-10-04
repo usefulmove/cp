ebb3f81a7a5ad732bd95885a26d80cef3e587321
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-03 20:01:23 -0700

    ...

--- leetcode/3475.py
+++ leetcode/3475.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
 def analyze_dna_patterns(samples: pd.DataFrame) -> pd.DataFrame:
+    samples['has_start'] = samples.dna_sequence.str.startswith('ATG').apply(int)
+    samples['has_stop'] = samples.dna_sequence.str.endswith('TAA').apply(int) + \
+        samples.dna_sequence.str.endswith('TAG').apply(int) + \
+        samples.dna_sequence.str.endswith('TGA').apply(int)
+    samples['has_atat'] = samples.dna_sequence.str.contains('ATAT').apply(int)
+    samples['has_ggg'] = samples.dna_sequence.str.contains('GGG').apply(int)
+
+    return samples.sort_values(by='sample_id', ascending=True)
+
+
+def analyze_dna_patterns(samples: pd.DataFrame) -> pd.DataFrame:
     def has_start(dna: str) -> int:
         return 1 if dna[:3] == "ATG" else 0
 
