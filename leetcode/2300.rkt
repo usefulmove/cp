552b60473401d85c7204d00023f7c84495b87944
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-07 19:04:33 -0700

    ...

---
+++ leetcode/2300.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (successful-pairs spells potions success)
+  (let* ((sorted-potions (sort potions <))
+         (num-potions (length potions))
+         (find-potions-ind (lambda (spell) ; binary search
+                             (let loop ((left 0)
+                                        (right (- num-potions 1)))
+                               (let* ((mid (floor (/ (+ left right) 2)))
+                                      (val (list-ref sorted-potions mid)))
+                                 (cond ((> left right) left) 
+                                       ((>= (* val spell) success) (loop left (- mid 1)))
+                                       (else (loop (+ mid 1) right))))))))
+    (map
+     (lambda (spell) (- num-potions (find-potions-ind spell)))
+     spells)))
+
+#;(define (successful-pairs spells potions success)
+  (map
+   (lambda (s)
+     (count
+      (lambda (p) (>= (* s p) success))
+      potions))
+   spells))
+
+(successful-pairs '(5 1 3) '(1 2 3 4 5) 7)
+(successful-pairs '(3 1 2) '(8 5 8) 16)
