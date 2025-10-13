967d8cc4848cefa833cc894309959ff8afaf0822
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-12 17:56:07 -0700

    ...

--- leetcode/2273.rkt
+++ leetcode/2273.rkt
@@ -1,14 +1,17 @@
 #lang racket
 
+#|
+Iterate through the list and check the first two words. If they are anagrams, remove the second and re-enter the iteration. If not, add the first to the accumulator and continue iterating through the rest. When you have reached the last word, add it to the accumulator and return the list of accumulated words.
+|#
+
 (define (remove-anagrams words)
-  (let* ((f-char-sort (lambda (ca cb)
-                        (< (char->integer ca)
-                           (char->integer cb))))
+  (let* ((f-csort (lambda (c_a c_b)
+                        (< (char->integer c_a)
+                           (char->integer c_b))))
          (anagrams? (lambda (word_a word_b)
-                      (equal? (sort (string->list word_a) f-char-sort)
-                              (sort (string->list word_b) f-char-sort)))))
-    (let loop ((ws words)
-               (acc '()))
+                      (equal? (sort (string->list word_a) f-csort)
+                              (sort (string->list word_b) f-csort)))))
+    (let loop ((ws words) (acc '()))
       (if (empty? (cdr ws))
           (reverse (cons (car ws) acc))
           (let ((w_a (car ws))
@@ -19,5 +22,5 @@
                 (loop (cdr ws)
                       (cons w_a acc))))))))
 
-(remove-anagrams '("abba" "baba" "bbaa" "cd" "cd")) ; => '("abba" "cd" 
+(remove-anagrams '("abba" "baba" "bbaa" "cd" "cd")) ; => '("abba" "cd")
 (remove-anagrams '("a" "b" "c" "d" "e")) ; => '("a" "b" "c" "d" "e")
