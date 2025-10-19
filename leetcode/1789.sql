d13ad4bb6633e0ed94851ed95bbe20ca7c3e0562
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-18 18:40:18 -0700

    ...

--- leetcode/1789.sql
+++ leetcode/1789.sql
@@ -1,9 +1,17 @@
 select employee_id, department_id
 from Employee
-where primary_flag = 'Y'
-union
-select employee_id, department_id
-from Employee
-where employee_id not in (select employee_id
-                          from Employee
-                          where primary_flag = 'Y')
+where primary_flag = 'Y or 'employee_id in (select employee_id
+                                            from Employee
+                                            group by employee_id
+                                            having count(*) = 1)
+
+
+--select employee_id, department_id
+--from Employee
+--where primary_flag = 'Y'
+--union
+--select employee_id, department_id
+--from Employee
+--where employee_id not in (select employee_id
+--                          from Employee
+--                          where primary_flag = 'Y')
