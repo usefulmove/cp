901afa28d864e34df30d05bb9461096094dc94eb
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-21 21:05:53 -0700

    ...

--- leetcode/3642.sql
+++ leetcode/3642.sql
@@ -1,23 +1,29 @@
-with summary as (
-  select
-    book_id,
-    sum(case when session_rating <= 2 then 1 else 0 end) as extreme_lows,
-    sum(case when session_rating >= 4 then 1 else 0 end) as extreme_highs,
-    count(*) as ratings,
-    max(session_rating) - min(session_rating) as rating_spread,
-    sum(case when session_rating <= 2 or session_rating >= 4 then 1 else 0 end) as extreme_ratings
+with extreme_added as (
     from reading_sessions
-   group by book_id),
-
-  for_join as (
     select
-      book_id,
-      rating_spread,
-      round(extreme_ratings * 1.0 / ratings, 2) as polarization_score
-      from summary
-     where ratings >= 5 and extreme_lows > 0 and extreme_highs > 0)
-
-select *
-  from books
-       inner join for_join using (book_id)
- order by polarization_score desc, title desc ;
+        *,
+        extreme_rating: (case when session_rating >= 4 or session_rating <= 2 then 1 else 0 end)
+),
+rating_summary as (
+    from extreme_added
+    select
+        book_id,
+        num_ratings: count(*),
+        num_extreme: sum(extreme_rating),
+        min_rating: min(session_rating),
+        max_rating: max(session_rating)
+    group by book_id
+)
+from books b
+    right join rating_summary r on b.book_id = r.book_id
+select
+    b.book_id,
+    title,
+    author,
+    genre,
+    pages,
+    rating_spread: max_rating - min_rating,
+    polarizaton_score: num_extreme / num_ratings
+where
+    num_ratings >= 5 and
+    min_rating <= 2 and max_rating >= 4
