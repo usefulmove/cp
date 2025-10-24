7cb423179eb89d8551ea3cf75465d3f7e6bfd131
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-23 19:09:00 -0700

    ...

--- leetcode/3497.sql
+++ leetcode/3497.sql
@@ -1,33 +1,21 @@
-with durations as (
-  with paid_users as (
-    with paid as (
-      select
-        user_id,
-        sum(case when activity_type = 'paid' then 1 else 0 end)
-          as ever_paid
-        from UserActivity
-       group by user_id)
+with aggregation as (
+    select user_id,
+           avg(case
+               when activity_type = 'free_trial' then activity_duration
+               else null
+               end
+           ) as trial_avg_duration,
+           avg(case
+               when activity_type = 'paid' then activity_duration
+               else null
+               end
+           ) as paid_avg_duration
+    from UserActivity
+    group by user_id)
 
-    select user_id 
-      from paid
-     where ever_paid > 0)
-
-  select
-    user_id,
-    (case when activity_type = 'free_trial' then activity_duration else null end)
-      as trial_duration,
-    (case when activity_type = 'paid' then activity_duration else null end)
-      as paid_duration
-    from
-      UserActivity
-   where
-    user_id in (select user_id from paid_users))
-
-select
-  user_id,
-  round(avg(trial_duration), 2) as trial_avg_duration,
-  round(avg(paid_duration), 2) as paid_avg_duration
-  from
-    durations
- group by user_id
- order by user_id
+select user_id,
+       round(trial_avg_duration, 2) as trial_avg_duration,
+       round(paid_avg_duration, 2)  as paid_avg_duration
+from aggregation
+where paid_avg_duration is not null
+order by user_id
