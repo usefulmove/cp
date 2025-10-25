dba9f96d48e946fa3e68df0fdbae94965f1f97ef
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-24 18:40:58 -0700

    ...

--- leetcode/1716.py
+++ leetcode/1716.py
@@ -1,7 +1,6 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
         b: int = 7
-        q: int = n // b
-        r: int = n % b
+        q, r = divmod(n, b)
 
         return (2 * q * r + r * r + r + q * b * b + q * q * b) // 2
