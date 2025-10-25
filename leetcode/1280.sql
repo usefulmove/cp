07accf35214f0b5a53cc6f6cf82687bd44862526
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-24 23:33:50 -0700

    ...

---
+++ leetcode/1280.sql
@@ -0,0 +1,21 @@
+with cross_prod as (select *
+                      from Students
+                           cross join Subjects),
+
+  cross_exam as (select c.student_id,
+                        c.student_name,
+                        c.subject_name,
+                        e.student_id as valid
+                   from cross_prod c
+                        left join Examinations e
+                            on c.student_id = e.student_id
+                            and c.subject_name = e.subject_name)
+
+select student_id,
+       student_name,
+       subject_name,
+       count(valid) as attended_exams
+  from cross_exam
+ group by student_id,
+          student_name,
+          subject_name
