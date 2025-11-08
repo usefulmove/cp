cb16ea07d854dc1611f911e00ba718804b5bbf99
Duane Edmonds <duane.edmonds@gmail.com>
2025-11-07 17:34:32 -0800

    ...

--- leetcode/1251.sql
+++ leetcode/1251.sql
@@ -1,22 +1,14 @@
-with avg_prices as (
-        select
-            u.product_id,
-            round(sum(1. * u.units * p.price) / sum(u.units), 2)
-                as average_price
-        from
-            UnitsSold u
-        left join
-            Prices p on u.product_id = p.product_id
-                and u.purchase_date between p.start_date and p.end_date
-        group by
-            1
-)
-
 select
     pid.product_id,
-    coalesce(ap.average_price, 0) as average_price
+    round(coalesce(sum(1. * u.units * p.price) / sum(u.units), 0), 2)
+        as average_price
 from
-    avg_prices ap
+    UnitsSold u
+left join
+    Prices p on u.product_id = p.product_id
+                and u.purchase_date between p.start_date and p.end_date
 right join
-    (select distinct p.product_id from Prices p) pid
-        on ap.product_id = pid.product_id;
+    (select distinct product_id from Prices) pid
+        on pid.product_id = u.product_id
+group by
+    1;
