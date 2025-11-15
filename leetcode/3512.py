d8a261a06e0a0fbf5b195512919973df966e46f4
Duane Edmonds <duane.edmonds@gmail.com>
2025-11-14 22:23:47 -0800

    ...

--- leetcode/3512.py
+++ leetcode/3512.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        return reduce(lambda a, b: a + b, nums) % k
+        return sum(nums) % k
