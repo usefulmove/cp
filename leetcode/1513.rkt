de9586b7591c91fb67c14136e670ab6fd4ea05bd
Duane Edmonds <duane.edmonds@gmail.com>
2025-11-15 17:14:32 -0800

    ...

---
+++ leetcode/1513.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+#|
+Split the string to get only substrings containing continuous 1's. Use the triangular number formula (n(n+1)/2) and the length of each substring to determine the number of sub-substrings with all 1's. Sum all of the substring triangular number totals.
+
+[trangular number](https://en.wikipedia.org/wiki/Triangular_number)
+|#
+
+(define (num-sub s)
+  (let ((tng (lambda (n)
+               (/ (* n (+ n 1)) 2))))
+    (foldl
+     (lambda (ones acc)
+       (modulo
+        (+ acc (tng (string-length ones)))
+        1000000007))
+     0
+     (string-split s "0"))))
+
+(num-sub "0110111") ; => 9
+(num-sub "101")     ; => 2
+(num-sub "111111")  ; => 21
