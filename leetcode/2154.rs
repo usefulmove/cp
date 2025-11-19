1a08095c8cc5396fa31f0d1f9de70043c96a975b
Duane Edmonds <duane.edmonds@gmail.com>
2025-11-18 17:06:27 -0800

    ...

--- leetcode/2154.rs
+++ leetcode/2154.rs
@@ -1,7 +1,7 @@
 fn main() {
     fn find_final_value(nums: Vec<i32>, original: i32) -> i32 {
         if nums.contains(&original) {
-            find_final_value(nums, original)
+            find_final_value(nums, original * 2)
         } else {
             original
         }
