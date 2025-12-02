1d137b4a5464a893f60713ddfb0a9829373d6026
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-01 17:22:23 -0800

    ...

--- leetcode/1021.rkt
+++ leetcode/1021.rkt
@@ -1,25 +1,30 @@
 #lang racket
 
+#|
+    Iterate through the list of characters and keep track of
+    the nesting depth and the accumulated output based on the
+    depth.
+ |#
+
 (define (remove-outer-parentheses s)
   (let loop ((cs (string->list s))
              (depth 0)
              (out '()))
-    (if (empty? cs)
-        (list->string (reverse out))
-        (cond ((and (= 0 depth) 
-                    (equal? #\( (car cs))) (loop (cdr cs)
-                                                 (+ depth 1)
-                                                 out))
-              ((and (= 1 depth) 
-                    (equal? #\) (car cs))) (loop (cdr cs)
-                                                 (- depth 1)
-                                                 out))
-              ((equal? #\( (car cs)) (loop (cdr cs)
-                                           (+ depth 1)
-                                           (cons #\( out)))
-              (else (loop (cdr cs)
-                          (- depth 1)
-                          (cons #\) out)))))))
+    (cond ((empty? cs) (list->string (reverse out)))
+          ((and (= 0 depth) 
+                (equal? #\( (car cs))) (loop (cdr cs)
+                                             (+ depth 1)
+                                             out))
+          ((and (= 1 depth) 
+                (equal? #\) (car cs))) (loop (cdr cs)
+                                             (- depth 1)
+                                             out))
+          ((equal? #\( (car cs)) (loop (cdr cs)
+                                       (+ depth 1)
+                                       (cons #\( out)))
+          (else (loop (cdr cs)
+                      (- depth 1)
+                      (cons #\) out))))))
 
 
 (remove-outer-parentheses "(()())(())")         ; => "()()()"
