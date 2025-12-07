812a872c8c03f8a7f792ff788f99fc144b4c989b
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-06 16:34:21 -0800

    ...

--- leetcode/1523.py
+++ leetcode/1523.py
@@ -1,11 +1,18 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        def odd(num):
-            return num % 2 == 1
-
         count_nums = high - low + 1
 
-        if odd(low) and odd(count_nums):
-            return count_nums // 2 + 1
-        else:
-            return count_nums // 2
+        return count_nums // 2 + (1 if low % 2 and high % 2 else 0)
+
+
+#class Solution:
+#    def countOdds(self, low: int, high: int) -> int:
+#        def odd(num):
+#            return num % 2 == 1
+#
+#        count_nums = high - low + 1
+#
+#        if odd(low) and odd(count_nums):
+#            return count_nums // 2 + 1
+#        else:
+#            return count_nums // 2
