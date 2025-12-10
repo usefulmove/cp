bb9df427fe4e374abbe2fd0fbaf3e9d43dd2bbee
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-09 18:18:33 -0800

    ...

---
+++ leetcode/3769.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortByReflection(self, nums: List[int]) -> List[int]:
+        def reflect(n):
+            return (int(format(f"{n:b}")[::-1]), n)
+
+        outs = sorted(nums, key=reflect)
+
+        return outs
