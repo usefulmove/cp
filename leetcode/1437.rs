354fa63805c0668f85789a3d8ceb19dd24d53b48
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-18 08:49:23 -0800

    ...

--- leetcode/1437.rs
+++ leetcode/1437.rs
@@ -2,22 +2,10 @@ fn main() {
     fn k_length_apart(nums: Vec<i32>, k: i32) -> bool {
         -1 != nums.iter().fold(k, |cnt, n|
             match n {
-                0 => {
-                    if cnt != -1 {
-                        cnt + 1
-                    } else {
-                        -1
-                    }
-                }
-                1 => {
-                    if cnt >= k {
-                        0
-                    } else {
-                        -1
-                    }
-                }
+                0 => if cnt != -1 { cnt + 1 } else { -1 }
+                1 => if cnt >= k { 0 } else { -1 }
                 _ => cnt
-        })
+            })
     }
 
     println!("{}", k_length_apart(vec![1, 0, 0, 0, 1, 0, 0, 1], 2)); // => true
