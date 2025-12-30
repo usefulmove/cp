25f9f1391445f808d74d35243843760ddaa6c116
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-29 17:54:16 -0800

    ...

---
+++ leetcode/3248.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+int finalPositionOfSnake(int n, const std::vector<std::string>& commands) {
+    return std::accumulate(commands.begin(), commands.end(), 0,
+        [n](int pos, const std::string& cmd) {
+            if (cmd == "UP") return pos - n;
+            if (cmd == "DOWN") return pos + n;
+            if (cmd == "RIGHT") return pos + 1;
+            if (cmd == "LEFT") return pos - 1;
+            return pos;
+        }
+    );
+}
