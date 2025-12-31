bbef8aa7103e53110a37b6d5560adc7ecfcd68f8
Duane Edmonds <duane.edmonds@gmail.com>
2025-12-30 17:13:57 -0800

    ...

--- leetcode/3536.py
+++ leetcode/3536.py
@@ -2,29 +2,31 @@
 
 
 class Solution:
-    def maxProduct(n: int) -> int:
-        def recur(num: int, a: int = 0, b: int = 0) -> int:
-            """
-            Iterate through the digits of NUM and keep track of the
-            two largest digits then return their product.
-            """
-            if num == 0:
-                return a * b
+    def maxProduct(self, n: int) -> int:
+        return self.recur(n)
 
-            quotient, digit = divmod(num, 10)
+    def recur(self, num: int, a: int = 0, b: int = 0) -> int:
+        """
+        Iterate through the digits of NUM and keep track of the
+        two largest digits then return their product.
+        """
+        if num == 0:
+            return a * b
 
-            match digit:
-                case digit if digit >= b:
-                    return recur(quotient, b, digit)
-                case digit if digit >= a:
-                    return recur(quotient, digit, b)
-                case _:
-                    return recur(quotient, a, b)
+        quotient, digit = divmod(num, 10)
+
+        match digit:
+            case digit if digit >= b:
+                return self.recur(quotient, b, digit)
+            case digit if digit >= a:
+                return self.recur(quotient, digit, b)
+            case _:
+                return self.recur(quotient, a, b)
 
 
-        return recur(n)
+s = Solution()
 
 
-print(Solution.maxProduct(31))
-print(Solution.maxProduct(22))
-print(Solution.maxProduct(124))
+print(s.maxProduct(31))
+print(s.maxProduct(22))
+print(s.maxProduct(124))
