9ac2c9b3fbc97e41b87cdd4c59d35f8fdf9aeb16
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-03 15:14:36 -0800

    ...

--- leetcode/3731.cpp
+++ leetcode/3731.cpp
@@ -10,14 +10,17 @@ class Solution {
         const auto min = std::min_element(nums.begin(), nums.end());
         const auto max = std::max_element(nums.begin(), nums.end());
 
-        out = std::ranges::fold_left(std::views::iota(*min, *max + 1), out, [&nums](std::vector<int> acc, int num) {
-            if (std::find(nums.begin(), nums.end(), num) == nums.end()) {
-                acc.push_back(num);
+        out = std::ranges::fold_left(
+            std::views::iota(*min, *max + 1), out,
+            [&nums](std::vector<int> acc, int num) {
+                if (std::find(nums.begin(), nums.end(), num) == nums.end()) {
+                    acc.push_back(num);
+                    return acc;
+                }
+
                 return acc;
             }
-
-            return acc;
-        });
+        );
 
         return out;
     }
