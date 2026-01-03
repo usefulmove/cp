73e9a5e8b4d470f769aad5ed77c715d63c9f3bd9
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-02 23:04:51 -0800

    ...

--- leetcode/3731.cpp
+++ leetcode/3731.cpp
@@ -4,15 +4,15 @@
 
 class Solution {
 public:
-  std::vector<int> findMissingElements(std::vector<int> &nums) {
-    std::vector<int> out;
+  vector<int> findMissingElements(vector<int> &nums) {
+    vector<int> out;
 
-    auto min = std::min_element(nums.begin(), nums.end());
-    auto max = std::max_element(nums.begin(), nums.end());
+    auto min = min_element(nums.begin(), nums.end());
+    auto max = max_element(nums.begin(), nums.end());
 
-    for (int n : std::ranges::views::iota(*min, *max + 1)) {
-      if (std::find(nums.begin(), nums.end(), n) == nums.end()) {
-        out.push_back(n);
+    for (int num : views::iota(*min, *max + 1)) {
+      if (find(nums.begin(), nums.end(), num) == nums.end()) {
+        out.push_back(num);
       }
     }
 
