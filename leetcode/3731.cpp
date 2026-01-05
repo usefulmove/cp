082b2e6acfe84992695cd2a50a6023d470865171
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-04 18:40:29 -0800

    ...

--- leetcode/3731.cpp
+++ leetcode/3731.cpp
@@ -6,23 +6,19 @@ class Solution {
   public:
     std::vector<int> findMissingElements(std::vector<int> &nums) {
         std::vector<int> out;
+        const auto [min, max] = std::ranges::minmax(nums);
 
-        const auto min = std::min_element(nums.begin(), nums.end());
-        const auto max = std::max_element(nums.begin(), nums.end());
-
-        out = std::ranges::fold_left(
-            std::views::iota(*min, *max + 1), out,
+        return std::ranges::fold_left(
+            std::views::iota(min, max + 1),
+            out,
             [&nums](std::vector<int> acc, int num) {
                 if (std::find(nums.begin(), nums.end(), num) == nums.end()) {
                     acc.push_back(num);
-                    return acc;
                 }
 
                 return acc;
             }
         );
-
-        return out;
     }
 };
 
