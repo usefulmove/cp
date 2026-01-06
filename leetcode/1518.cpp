38f357215a5d2bec0b9754c86b4791609fe39852
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-06 02:30:31 -0800

    ...

---
+++ leetcode/1518.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        auto loop = [numExchange](this auto &&self, int empties,
+                                  int drunks) -> int {
+            if (empties >= numExchange)
+                return self(empties - numExchange + 1, drunks + 1);
+            
+            return drunks;
+        };
+
+        return loop(numBottles, numBottles);
+    }
+};
