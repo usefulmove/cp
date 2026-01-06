3b0648c26cd65770b2f1f4fe73d7dda7e04289c7
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-05 19:11:26 -0800

    ...

--- leetcode/2956.cpp
+++ leetcode/2956.cpp
@@ -1,24 +1,22 @@
 class Solution {
-public:
-    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        int onesintwo = std::ranges::fold_left(
-            nums1, 0,
-            [nums2](int acc, int num1) {
+  public:
+    vector<int> findIntersectionValues(vector<int> &nums1, vector<int> &nums2) {
+        int onesintwo =
+            std::ranges::fold_left(nums1, 0, [nums2](int acc, int num1) {
                 if (std::ranges::contains(nums2, num1))
                     return acc + 1;
-    
+
                 return acc;
             });
 
-        int twosinone = std::ranges::fold_left(
-            nums2, 0,
-            [nums1](int acc, int num2) {
+        int twosinone =
+            std::ranges::fold_left(nums2, 0, [nums1](int acc, int num2) {
                 if (std::ranges::contains(nums1, num2))
                     return acc + 1;
-    
+
                 return acc;
             });
 
-        return { onesintwo, twosinone };
+        return {onesintwo, twosinone};
     }
 };
--- leetcode/3736.cpp
+++ leetcode/3736.cpp
@@ -1,13 +1,11 @@
 class Solution {
   public:
     int minMoves(vector<int> &nums) {
-        const int max_n = std::ranges::fold_left(nums, 0, [](int a, int b) {
-            return std::max(a, b);
-        });
+        const int max_n = std::ranges::fold_left(
+            nums, 0, [](int a, int b) { return std::max(a, b); });
 
-        const int res = std::ranges::fold_left(nums, 0, [max_n](int acc, int n) {
-            return acc + max_n - n;
-        });
+        const int res = std::ranges::fold_left(
+            nums, 0, [max_n](int acc, int n) { return acc + max_n - n; });
 
         return res;
     }
