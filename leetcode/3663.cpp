4594dd53dae43b0174ac9d307f065039d269a916
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-05 20:27:39 -0800

    ...

---
+++ leetcode/3663.cpp
@@ -0,0 +1,26 @@
+#include <print>
+
+class Solution {
+  public:
+    int getLeastFrequentDigit(int n) {
+        std::array<int, 10> cnts = { 0 };
+
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+            cnts[digit] += 1;
+        }
+
+        int lf_digit = 0;
+        int minimum = 999;
+
+        for (auto const [dig, cnt] : std::views::enumerate(cnts)) {
+            if ((cnt != 0) && (cnt < minimum)) {
+                lf_digit = dig;
+                minimum = cnt;
+            }
+        }
+
+        return lf_digit;
+    }
+};
