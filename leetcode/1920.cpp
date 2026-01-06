4612ff609f64e7dd6eee1184a118ce9036a9d733
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-06 15:46:58 -0800

    ...

--- leetcode/1920.cpp
+++ leetcode/1920.cpp
@@ -1,8 +1,8 @@
 class Solution {
   public:
-    vector<int> buildArray(vector<int>& nums) {
-        return nums
-            | std::views::transform([nums](int n) -> int { return nums[n]; })
-            | std::ranges::to<vector>();
+    vector<int> buildArray(vector<int> &nums) {
+        return nums |
+               std::views::transform([nums](int n) -> int { return nums[n]; }) |
+               std::ranges::to<vector>();
     }
 };
