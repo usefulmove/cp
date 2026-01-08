1e0d894d8c3d7845c3efa2bc1fc38de60aad2cdd
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-08 01:03:24 -0800

    ...

--- leetcode/3712.cpp
+++ leetcode/3712.cpp
@@ -1,10 +1,10 @@
 class Solution {
   public:
-    int sumDivisibleByK(const vector<int> &nums, const int k) {
+    int sumDivisibleByK(const vector<int> &nums, const int &k) {
         std::array<int, 101> cnts = {0};
 
-        for (const int n : nums) {
-            cnts[n] += 1;
+        for (const int &n : nums) {
+            cnts[n]++;
         }
 
         return std::ranges::fold_left(std::ranges::views::enumerate(cnts), 0,
