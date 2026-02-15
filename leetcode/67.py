3018b51b2dd5ef41d1d0f5f52d02c8eb1f378c56
Duane Edmonds <duane.edmonds@gmail.com>
2026-02-14 21:30:23 -0800

    ...

---
+++ leetcode/67.py
@@ -0,0 +1,32 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        n: int = max(len(a), len(b)) + 1
+
+        def recur(sa: str, sb: str, acc: str = "", carry: str = "0"):
+            if not sa:
+                out = acc[::-1].lstrip('0')
+                return out if out else "0"
+
+            bit_a, *rest_a = sa
+            bit_b, *rest_b = sb
+
+            match int(bit_a) + int(bit_b) + int(carry):
+                case 0:
+                    bit = carry = "0"
+                case 1:
+                    bit = "1"
+                    carry = "0"
+                case 2:
+                    bit = "0"
+                    carry = "1"
+                case 3:
+                    bit = carry = "1"
+
+            return recur(rest_a, rest_b, acc + bit, carry)
+
+        return recur(reversed(a.zfill(n)), reversed(b.zfill(n)))
+
+ans = Solution()
+print(f"{ans.addBinary("11", "1") = }")      # => "100"
+print(f"{ans.addBinary("1010", "1011") = }") # => "10101"
+print(f"{ans.addBinary("0", "0") = }")       # => "0"
