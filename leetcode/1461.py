9c7647e0b465ae59357340c1514b0b18b8fd0aaf
Duane Edmonds <duane.edmonds@gmail.com>
2026-02-22 17:12:45 -0800

    ...

--- leetcode/1461.py
+++ leetcode/1461.py
@@ -1,7 +1,14 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        for n in reversed(range(2**k)):
-            code = bin(n)[2:].zfill(k)
-            if not code in s:
-                return False
-        return True
+        codes = set()
+        for i in range(len(s) - k + 1):
+            codes.add(s[i:i+k])
+        return len(codes) == 2**k
+
+#class Solution:
+#    def hasAllCodes(self, s: str, k: int) -> bool:
+#        for n in reversed(range(2**k)):
+#            code = bin(n)[2:].zfill(k)
+#            if not code in s:
+#                return False
+#        return True
