877a79ed8cb1cd44a798ed5664c02f8de8b733a0
Duane Edmonds <duane.edmonds@gmail.com>
2026-02-25 00:33:43 -0800

    ...

--- leetcode/1769.py
+++ leetcode/1769.py
@@ -1,9 +1,21 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        n = len(boxes)
-        out = [0] * n
-        for i in range(n):
-            for j in range(n):
+        def ops(i: int) -> int:
+            out = 0
+            for j in range(len(boxes)):
                 if boxes[j] == '1':
-                    out[i] += abs(i - j)
-        return out
+                    out += abs(i - j)
+            return out
+
+        return [ops(i) for i, _ in enumerate(boxes)]
+
+
+#class Solution:
+#    def minOperations(self, boxes: str) -> List[int]:
+#        n = len(boxes)
+#        out = [0] * n
+#        for i in range(n):
+#            for j in range(n):
+#                if boxes[j] == '1':
+#                    out[i] += abs(i - j)
+#        return out
