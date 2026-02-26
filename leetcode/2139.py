c71fcfe5dfd55ded1bd72c34df65135c2bf42f42
Duane Edmonds <duane.edmonds@gmail.com>
2026-02-25 20:15:42 -0800

    ...

--- leetcode/2139.py
+++ leetcode/2139.py
@@ -1,8 +1,8 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int, moves: int = 0) -> int:
-        if target == 1:
+        if 1 == target:
             return moves
-        if 0 == target & 1 and maxDoubles > 0: # even with doubles
+        if 0 == target & 1 and maxDoubles: # even with doubles
             return self.minMoves(target // 2, maxDoubles - 1, moves + 1)
         if 0 == target & 1: # even without doubles
             return self.minMoves(1, 0, moves + target - 1)
