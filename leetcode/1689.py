6a8e488d685e154160038b349051eb25514b752d
Duane Edmonds <duane.edmonds@gmail.com>
2026-02-28 17:15:06 -0800

    ...

--- leetcode/1689.py
+++ leetcode/1689.py
@@ -1,3 +1,14 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        return max(int(c) for c in n)
+        for a in range(9, 0, -1):
+            if str(a) in n:
+                return a
+        return 0
+
+#class Solution:
+#    def minPartitions(self, n: str) -> int:
+#        return int(max(n))
+
+#class Solution:
+#    def minPartitions(self, n: str) -> int:
+#        return max(int(c) for c in n)
