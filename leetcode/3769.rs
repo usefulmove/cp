193115fd36d8b719da987f03335ceb8e540f0215
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-04 22:49:17 -0800

    ...

--- leetcode/3769.rs
+++ leetcode/3769.rs
@@ -1,19 +1,34 @@
 impl Solution {
     pub fn sort_by_reflection(mut nums: Vec<i32>) -> Vec<i32> {
         let mut sorted = nums;
-        sorted.sort_by(|a, b| {
-            let a_binary_str: String = format!("{:b}", a);
-            let b_binary_str: String = format!("{:b}", b);
+        sorted.sort_by_key(|n| {
+            let binary_str: String = format!("{:b}", n);
+            let binary_str_rev: String = binary_str.chars().rev().collect();
+            let reflection = i32::from_str_radix(&binary_str_rev, 2).unwrap();
 
-            let a_binary_str_rev: String = a_binary_str.chars().rev().collect();
-            let b_binary_str_rev: String = b_binary_str.chars().rev().collect();
-
-            let a_reflection = i32::from_str_radix(&a_binary_str_rev, 2).unwrap();
-            let b_reflection = i32::from_str_radix(&b_binary_str_rev, 2).unwrap();
-
-            a_reflection.cmp(&b_reflection)
-                .then_with(|| a.cmp(b))
+            (reflection, n.clone())
         });
         sorted
     }
 }
+
+
+//impl Solution {
+//    pub fn sort_by_reflection(mut nums: Vec<i32>) -> Vec<i32> {
+//        let mut sorted = nums;
+//        sorted.sort_by(|a, b| {
+//            let a_binary_str: String = format!("{:b}", a);
+//            let b_binary_str: String = format!("{:b}", b);
+//
+//            let a_binary_str_rev: String = a_binary_str.chars().rev().collect();
+//            let b_binary_str_rev: String = b_binary_str.chars().rev().collect();
+//
+//            let a_reflection = i32::from_str_radix(&a_binary_str_rev, 2).unwrap();
+//            let b_reflection = i32::from_str_radix(&b_binary_str_rev, 2).unwrap();
+//
+//            a_reflection.cmp(&b_reflection)
+//                .then_with(|| a.cmp(b))
+//        });
+//        sorted
+//    }
+//}
