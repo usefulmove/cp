b89f0129283e48d516d9b785ed708a1897022d86
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-08 18:37:52 -0700

    ...

--- leetcode/3861.py
+++ leetcode/3861.py
@@ -1,13 +1,27 @@
 class Solution:
     def minimumIndex(self, capacity: list[int], itemSize: int) -> int:
-        residual_capacities = [c-itemSize for c in capacity]
-
         min_idx = -1
         min_cap = 101
-        for i, cap in enumerate(residual_capacities):
-            if cap < 0:
+        for i, cap in enumerate(capacity):
+            room = cap-itemSize
+            if room < 0:
                 continue
-            if cap < min_cap:
-                min_idx, min_cap = i, cap
+            if room < min_cap:
+                min_idx, min_cap = i, room
         
         return min_idx
+
+
+#class Solution:
+#    def minimumIndex(self, capacity: list[int], itemSize: int) -> int:
+#        residual_capacities = [c-itemSize for c in capacity]
+#
+#        min_idx = -1
+#        min_cap = 101
+#        for i, cap in enumerate(residual_capacities):
+#            if cap < 0:
+#                continue
+#            if cap < min_cap:
+#                min_idx, min_cap = i, cap
+#        
+#        return min_idx
