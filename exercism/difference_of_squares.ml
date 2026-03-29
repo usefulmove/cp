2e0e92419bad4fc495a83f8da8f77f567f5729eb
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-28 20:15:57 -0700

    ...

---
+++ exercism/difference_of_squares.ml
@@ -0,0 +1,13 @@
+let square_of_sum n = 
+    let sum = List.fold_left (fun a b -> a + b)
+                             0
+                             (List.init (n + 1) Fun.id)
+    in sum * sum
+
+let sum_of_squares n =
+    List.fold_left (fun acc a -> acc + a * a)
+                   0
+                   (List.init (n + 1) Fun.id)
+
+let difference_of_squares n =
+    (square_of_sum n) - (sum_of_squares n)
