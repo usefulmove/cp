63a92741fadee39a5e31ee9134d90f0f0d54c20a
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-30 23:10:47 -0700

    ...

--- exercism/armstrong_numbers.ml
+++ exercism/armstrong_numbers.ml
@@ -1,12 +1,18 @@
 let validate candidate =
-    let digits =
-        List.map Char.Ascii.digit_to_int ((string_of_int candidate) |> String.to_seq |> List.of_seq) in
-    let n =
-        String.length (string_of_int candidate) in
-    let armstrong_sum =
-        List.fold_left (fun acc a -> acc +. (float_of_int a) ** (float_of_int n)) 0.0 digits in
-    candidate = int_of_float armstrong_sum
+  let candidate_string = string_of_int candidate in
+  let digits =
+    List.map Char.Ascii.digit_to_int
+      (candidate_string |> String.to_seq |> List.of_seq)
+  in
+  let n = String.length candidate_string in
+  let rec power_int base exp =
+    match exp with 1 -> base | _ -> base * power_int base (exp - 1)
+  in
+  let armstrong_sum =
+    List.fold_left (fun acc a -> acc + power_int a n) 0 digits
+  in
+  candidate = armstrong_sum
 
-let _ = print_endline (string_of_bool (validate 8))
-let _ = print_endline (string_of_bool (validate 10))
-let _ = print_endline (string_of_bool (validate 153))
+let _ = print_endline (string_of_bool (validate 8)) (* true *)
+let _ = print_endline (string_of_bool (validate 10)) (* false *)
+let _ = print_endline (string_of_bool (validate 153)) (* true *)
