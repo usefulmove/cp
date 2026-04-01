aad6064a90e27559b32911486d57b03ca03b5fba
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-31 20:32:59 -0700

    ...

--- exercism/collatz_conjecture.ml
+++ exercism/collatz_conjecture.ml
@@ -1,11 +1,13 @@
 let collatz_conjecture n =
   let rec helper num cnt =
-    if num <= 0 then Error "Only positive integers are allowed"
-    else
-      match num with
-      | 1 -> Ok cnt
-      | a ->
-          if a mod 2 = 0 then helper (a / 2) (cnt + 1)
-          else helper ((3 * a) + 1) (cnt + 1)
+    match num <= 0 with
+    | true -> Error "Only positive integers are allowed"
+    | false -> (
+        match num with
+        | 1 -> Ok cnt
+        | a -> (
+            match a mod 2 with
+            | 0 -> helper (a / 2) (cnt + 1)
+            | 1 -> helper (3 * a + 1) (cnt + 1)))
   in
   helper n 0
