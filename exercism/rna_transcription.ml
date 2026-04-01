33fae14d2b39e45c6438bfca41ca2563a85251ab
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-31 20:12:00 -0700

    ...

---
+++ exercism/rna_transcription.ml
@@ -0,0 +1,15 @@
+type dna = [ `A | `C | `G | `T ]
+type rna = [ `A | `C | `G | `U ]
+
+let to_rna = List.map (function `G -> `C | `C -> `G | `T -> `A | `A -> `U)
+
+(*
+let to_rna dna_strand =
+    let f = function
+        | `G -> `C
+        | `C -> `G
+        | `T -> `A
+        | `A -> `U
+    in
+    List.map f dna_strand
+*)
