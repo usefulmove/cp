ea76991335ce2ce512386e201d28c071504703a1
Duane Edmonds <duane.edmonds@gmail.com>
2026-04-09 20:09:47 -0700

    ...

--- leetcode/3740.py
+++ leetcode/3740.py
@@ -1,15 +1,13 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         n = len(nums)
-        default = 2**31 - 1
-        min_distance = default
-        def calculate_distance(a, b, c):
-            return (b - a) + (c - b) + (c - a)
+        min_distance = default = 200
+        distance = lambda a, b, c: (c - a) << 1
         
         for i in range(n-2):
             for j in range(i+1, n-1):
                 for k in range(j+1, n):
                     if nums[i] == nums[j] == nums[k]:
-                        min_distance = min(min_distance, calculate_distance(i, j, k))
+                        min_distance = min(min_distance, distance(i, j, k))
 
         return -1 if min_distance == default else min_distance
