deaf3e8c44c4f15f0c2633565e21f81bba4c627c
Duane Edmonds <duane.edmonds@gmail.com>
2026-04-11 20:44:19 -0700

    ...

---
+++ leetcode/3194.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <climits>
+
+class Solution {
+  public:
+    double minimumAverage(vector<int>& nums) {
+        int len = nums.size();
+        std::sort(nums.begin(), nums.end());
+        int i = 0;
+        int min_sum = INT_MAX;
+
+        while (i <= len-i-1) {
+            int sum = nums[i] + nums[len-i-1];
+            if (sum < min_sum) { min_sum = sum; }
+            i++;
+        }
+
+        return min_sum / 2.0;
+    }
+};
