88e3382120b20c02153b4e0baa927fd1ebd8d878
Duane Edmonds <duane.edmonds@gmail.com>
2026-06-07 17:08:11 -0700

    ...

---
+++ leetcode/2161.scala
@@ -0,0 +1,8 @@
+object Solution {
+    def pivotArray(nums: Array[Int], pivot: Int): Array[Int] = {
+        val (part1, part2) = nums partition { _ < pivot }
+        val (part2a, part2b) = part2 partition { _ == pivot }
+        
+        part1 :++ part2a :++ part2b
+    }
+}
