bcab111869a99d954c235ababb4f2b3dec8f208c
Duane Edmonds <duane.edmonds@gmail.com>
2026-06-07 21:36:20 -0700

    ...

--- leetcode/2149.py
+++ leetcode/2149.py
@@ -2,10 +2,8 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         def partition(predicate, xs):
             ps, ns = [], []
-
             for x in xs:
                 (ps if predicate(x) else ns).append(x)
-            
             return ps, ns
 
         pos, neg = partition(lambda n: n > 0, nums)
