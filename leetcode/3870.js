3b6f0d7eb147900a9721ca519fcfdabc69af7c77
Duane Edmonds <duane.edmonds@gmail.com>
2026-06-08 18:08:29 -0700

    ...

--- leetcode/3870.js
+++ leetcode/3870.js
@@ -2,7 +2,9 @@
  * @param {number} n
  * @return {number}
  */
-var countCommas = function(n) {
-    if (n < 1000) { return 0; }
-    return n - 999;
-};
+var countCommas = n => (n < 1000) ? 0 : n - 999;
+
+//var countCommas = function(n) {
+//    if (n < 1000) { return 0; }
+//    return n - 999;
+//};
