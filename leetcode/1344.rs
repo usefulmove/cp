ea76bf9ab38d73eb530ce8bd27805870d9c0e0b2
Duane Edmonds <duane.edmonds@gmail.com>
2026-06-17 17:32:34 -0700

    ...

--- leetcode/1344.rs
+++ leetcode/1344.rs
@@ -1,10 +1,9 @@
 impl Solution {
     pub fn angle_clock(hour: i32, minutes: i32) -> f64 {
-        let mang = 6. * minutes as f64;
-        let hang = 30. * (hour % 12) as f64 + mang / 12.;
+        let min_ang = 6. * minutes as f64;
+        let hour_ang = 30. * (hour % 12) as f64 + min_ang / 12.;
+        let diff = (hour_ang - min_ang).abs();
         
-        let diff = hang - mang;
-        
-        diff.abs().min(360.-diff.abs())
+        diff.min(360. - diff)
     }
 }
