182d1ec0f7203a34dbe77a3dd4cf8732e4feb432
usefulmove <duane.edmonds@gmail.com>
2026-08-17 18:44:04 -0700

    ...

--- leetcode/4006.py
+++ leetcode/4006.py
@@ -1,16 +1,13 @@
 class Solution:
     def countValidPrefixes(self, s: str) -> int:
-        cnt = 0
-        ones = 0
-        zeros = 0
+        cnt = ones = zeros = 0
 
-        for bit in s:
-            match bit:
+        for b in s:
+            match b:
                 case '0':
                     zeros += 1
                 case '1':
                     ones += 1
-            
             if abs(ones - zeros) < 2:
                 cnt += 1
         
