a105f2e22db47a5909e4f14cdd5afc4791c5b67f
usefulmove <duane.edmonds@gmail.com>
2026-09-18 23:30:17 -0700

    ...

---
+++ leetcode/49.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val map: MutableMap<String, List<String>> = mutableMapOf()
+        
+        for (str in strs) {
+            val key: String = str.toList().sorted().joinToString("")
+            map[key] = map.getOrDefault(key, listOf()) + str
+        }
+
+        val out: MutableList<List<String>> = mutableListOf(listOf())
+        for (key in map.keys) {
+            out.add(map[key]!!)
+        }
+        
+        return out.toList().filter { !it.isEmpty() }
+    }
+}
