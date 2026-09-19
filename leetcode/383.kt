a450b73c833f477d3d88c9419354ba4810b31c55
usefulmove <duane.edmonds@gmail.com>
2026-09-19 15:29:55 -0700

    ...

--- leetcode/383.kt
+++ leetcode/383.kt
@@ -8,14 +8,27 @@ class Solution {
         return true
     }
 
+    fun counter(s: String): Map<Char, Int> = s.groupingBy { it }.eachCount()
+}
+
+/*
+class Solution {
+    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+        val rCounts: Map<Char, Int> = counter(ransomNote)
+        val mCounts: Map<Char, Int> = counter(magazine)
+        for (c in rCounts.keys)
+            if (rCounts.getOrDefault(c, 0) > mCounts.getOrDefault(c, 0))
+                return false
+        return true
+    }
+
     fun counter(s: String): Map<Char, Int> {
-        var m = mutableMapOf<Char, Int>()
+        val m = mutableMapOf<Char, Int>()
         for (c in s) m[c] = m.getOrDefault(c, 0) + 1
         return m
     }
 }
 
-/*
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
         if (ransomNote.isEmpty()) return true
