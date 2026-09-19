f4ead99f42452910abe3f640830b3a168a7adfda
usefulmove <duane.edmonds@gmail.com>
2026-09-18 21:13:08 -0700

    ...

--- leetcode/383.kt
+++ leetcode/383.kt
@@ -13,3 +13,18 @@ class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+        fun loop(rs: List<Char>, ms: List<Char>): Boolean {
+            if (rs.isEmpty()) return true
+            val c: Char = rs[0]
+            val rest: List<Char> = rs.slice(1..rs.lastIndex)
+            if (c in ms) return loop(rest, ms - c)
+            return false
+        }
+        return loop(ransomNote.toList(), magazine.toList())
+    }
+}
+ */
