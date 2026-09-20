0369c332c41aa90649f6930063377ebb9ff9b2f6
usefulmove <duane.edmonds@gmail.com>
2026-09-19 17:19:12 -0700

    ...

--- leetcode/509.kt
+++ leetcode/509.kt
@@ -1,13 +1,10 @@
 class Solution {
     fun fib(n: Int): Int {
-        fun fib2(n: Int, a: Int, b: Int): Int {
-          when (n) {
-            0 -> return a
-            1 -> return b
-            else -> return fib2(n-1, b, a+b)
-          }
-        }        
-        
+        tailrec fun fib2(n: Int, a: Int, b: Int): Int = when (n) {
+            0 -> a
+            1 -> b
+            else -> fib2(n - 1, b, a + b)
+        }
         return fib2(n, 0, 1)
     }
 }
--- leetcode/70.kt
+++ leetcode/70.kt
@@ -1,13 +1,10 @@
 class Solution {
     fun climbStairs(n: Int): Int {
-        fun fib(n: Int, a: Int, b: Int): Int {
-          when (n) {
-            0 -> return a
-            1 -> return b
-            else -> return fib(n-1, b, a+b)
-          }
+        tailrec fun fib(n: Int, a: Int, b: Int): Int = when (n) {
+            0 -> a
+            1 -> b
+            else -> fib(n - 1, b, a + b)
         }
-        
-        return fib(n+1, 0, 1)
+        return fib(n + 1, 0, 1)
     }
 }
