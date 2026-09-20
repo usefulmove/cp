efc0d66014448b13302825b24c0313391d5dd36b
usefulmove <duane.edmonds@gmail.com>
2026-09-19 17:48:48 -0700

    ...

--- leetcode/3498.kt
+++ leetcode/3498.kt
@@ -3,3 +3,17 @@ class Solution {
         .withIndex()
         .fold(0) { acc, (i, c) -> acc + (i + 1) * (26 - c.code + 'a'.code) }
 }
+
+/*
+class Solution {
+    fun reverseDegree(s: String): Int {
+        tailrec fun loop(cs: List<Char>, ind: Int = 1, sum: Int = 0): Int =
+            if (cs.isEmpty()) sum else loop(
+                cs.slice(1..cs.lastIndex),
+                ind + 1,
+                sum + ind * (26 - cs[0].code + 'a'.code)
+            )
+        return loop(s.toList())
+    }
+}
+ */
